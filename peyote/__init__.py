4a701325b38b4b8480d14b6993cd0f093d59d272
PaulE <paul.easter1@monash.edu>
2018-03-27 16:05:04 +1100

    Paul Easter: Fixed up module imports in parameter.py

--- peyote/__init__.py
+++ peyote/__init__.py
@@ -3,5 +3,5 @@ from __future__ import print_function, division
 # import local files
 import peyote.detector as detector
 import peyote.parameter as parameter
-import peyote.prior as prior
 import peyote.source as source
+import peyote.prior as prior
