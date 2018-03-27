47162b2fec0bb3622ac8157d68a413e17e035360
Colm Talbot <colm.talbot@ligo.org>
2018-03-27 14:46:47 +1100

    we think we understand antenna response

--- peyote/detector.py
+++ peyote/detector.py
@@ -1,3 +1,7 @@
+from __future__ import division, print_function
+import numpy as np
+import os
+
 class Interferometer:
     """Class for the Interferometer """
 
@@ -5,19 +9,52 @@ class Interferometer:
         """
         Interferometer class
         :param name: interferometer name, e.g., H1
-        :param x: unit vector along one arm in the geocentric frame
-        :param y: unit vector along the other arm in the geocentric frame
+        :param x: unit vector along one arm in Earth-centered cartesian coordinates
+        :param y: unit vector along the other arm in Earth-centered cartesian coordinates
         :param length: length of the interferometer
         """
         self.name = name
         self.x = x
         self.y = y
         self.length = length
+        self.D = 0.5*(np.tensordot(self.x,self.x,0)-np.tensordot(self.y,self.y,0))
         return None
 
-    def antenna_response(self):
-        '''sylvia does stuff'''
-        return None
+    def antenna_response(self, theta, phi, mode):
+        '''
+        Calculate the antenna response function for a given sky location
+
+        arXiv:0903.0528
+        TODO: argue about frames, relative to detector frame, earth-frame?
+
+        :param theta: angle from the north pole
+        :param phi: angle from the prime meridian
+        :param mode: polarisation mode
+        :return: f(theta, phi, mode): antenna response for the specified mode.
+        '''
+        m = np.array([np.sin(phi),-np.cos(phi),0])
+        n = np.array([np.cos(phi)*np.cos(theta),np.cos(theta)*np.sin(phi),-np.sin(theta)])
+        omega = np.cross(m, n)
+
+        if mode=="plus":
+                e = np.tensordot(m, m, 0)-np.tensordot(n, n, 0)
+        elif mode=="cross":
+                e = np.tensordot(m, n, 0)+np.tensordot(n, m, 0)
+        elif mode=="b":
+                e = np.tensordot(m, m, 0)+np.tensordot(n, n, 0)
+        elif mode=="l":
+                e = np.sqrt(2)*np.tensordot(omega, omega, 0)
+        elif mode=="x":
+                e = np.tensordot(m, omega, 0)+np.tensordot(omega, m, 0)
+        elif mode=="y":
+                e = np.tensordot(n, omega, 0)+np.tensordot(omega, n, 0)
+        else:
+                print("Not a polarization mode!")
+                return None
+
+        f = np.tensordot(self.D, e, axes=2)
+        return f
+
 
 class PowerSpectralDensity:
 
