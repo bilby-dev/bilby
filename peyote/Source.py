f364ad89895dde1ee22df3c529b99f2c41d71ef3
moritz <email@moritz-huebner.de>
2018-03-27 13:21:06 +1100

    Moritz Huebner: Added missing self arguments

--- peyote/Source.py
+++ peyote/Source.py
@@ -6,29 +6,31 @@ class Source:
 
 class Glitch(Source):
     def __init__(self, name):
-        Source.__init__(name)
+        Source.__init__(self, name)
 
 
 class AstrophysicalSource(Source):
     def __init__(self, name):
-        Source.__init__(name)
+        Source.__init__(self, name)
 
 
 class CompactBinaryCoalescence(AstrophysicalSource):
     def __init__(self, name):
-        AstrophysicalSource.__init__(name)
+        AstrophysicalSource.__init__(self, name)
 
 
 class Supernova(AstrophysicalSource):
     def __init__(self, name):
-        AstrophysicalSource.__init__(name)
+        AstrophysicalSource.__init__(self, name)
 
 
 class BinaryBlackHole(CompactBinaryCoalescence):
     def __init__(self, name):
-        CompactBinaryCoalescence.__init__(name)
+        CompactBinaryCoalescence.__init__(self, name)
 
 
 class BinaryBlackNeutronStar(CompactBinaryCoalescence):
     def __init__(self, name):
-        CompactBinaryCoalescence.__init__(name)
+        CompactBinaryCoalescence.__init__(self, name)
+
+
