659eda43fe9a510474a829a64021652541f05816
moritz <email@moritz-huebner.de>
2018-03-27 16:09:32 +1100

    Moritz Huebner: Did some refactoring

--- peyote/detector.py
+++ peyote/detector.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from scipy.interpolate import interp1d
 
+
 class Interferometer:
     """Class for the Interferometer """
 
@@ -18,7 +19,7 @@ class Interferometer:
         self.x = x
         self.y = y
         self.length = length
-        self.D = 0.5*(np.tensordot(self.x,self.x,0)-np.tensordot(self.y,self.y,0))
+        self.D = 0.5 * (np.tensordot(self.x, self.x, 0) - np.tensordot(self.y, self.y, 0))
         return None
 
     def antenna_response(self, theta, phi, mode):
@@ -33,25 +34,25 @@ class Interferometer:
         :param mode: polarisation mode
         :return: f(theta, phi, mode): antenna response for the specified mode.
         '''
-        m = np.array([np.sin(phi),-np.cos(phi),0])
-        n = np.array([np.cos(phi)*np.cos(theta),np.cos(theta)*np.sin(phi),-np.sin(theta)])
+        m = np.array([np.sin(phi), -np.cos(phi), 0])
+        n = np.array([np.cos(phi) * np.cos(theta), np.cos(theta) * np.sin(phi), -np.sin(theta)])
         omega = np.cross(m, n)
 
-        if mode=="plus":
-                e = np.tensordot(m, m, 0)-np.tensordot(n, n, 0)
-        elif mode=="cross":
-                e = np.tensordot(m, n, 0)+np.tensordot(n, m, 0)
-        elif mode=="b":
-                e = np.tensordot(m, m, 0)+np.tensordot(n, n, 0)
-        elif mode=="l":
-                e = np.sqrt(2)*np.tensordot(omega, omega, 0)
-        elif mode=="x":
-                e = np.tensordot(m, omega, 0)+np.tensordot(omega, m, 0)
-        elif mode=="y":
-                e = np.tensordot(n, omega, 0)+np.tensordot(omega, n, 0)
+        if mode == "plus":
+            e = np.tensordot(m, m, 0) - np.tensordot(n, n, 0)
+        elif mode == "cross":
+            e = np.tensordot(m, n, 0) + np.tensordot(n, m, 0)
+        elif mode == "b":
+            e = np.tensordot(m, m, 0) + np.tensordot(n, n, 0)
+        elif mode == "l":
+            e = np.sqrt(2) * np.tensordot(omega, omega, 0)
+        elif mode == "x":
+            e = np.tensordot(m, omega, 0) + np.tensordot(omega, m, 0)
+        elif mode == "y":
+            e = np.tensordot(n, omega, 0) + np.tensordot(omega, n, 0)
         else:
-                print("Not a polarization mode!")
-                return None
+            print("Not a polarization mode!")
+            return None
 
         f = np.tensordot(self.D, e, axes=2)
         return f
@@ -86,43 +87,37 @@ class PowerSpectralDensity:
         spectral_density = np.genfromtxt(sd_file)
         self.frequencies = spectral_density[:, 0]
         self.amplitude_spectral_density = spectral_density[:, 1]
-        self.power_spectral_density = self.amplitude_spectral_density**2
+        self.power_spectral_density = self.amplitude_spectral_density ** 2
 
     def noise_realisations(self, sampling_frequency, duration):
 
-        # calculate N = nuber of samples
-        N = duration * sampling_frequency
-        N = int(np.round(N))
+        number_of_samples = int(np.round(duration * sampling_frequency))
 
         # prepare for FFT
-        numFreqs = (N-1)//2
-        deltaF = 1./duration
+        number_of_frequencies = (number_of_samples - 1) // 2
+        delta_f = 1. / duration
 
-        f = deltaF*np.linspace(1, numFreqs, numFreqs)
+        f = self.equally_spaced_frequency_array(delta_f, number_of_frequencies)
 
-        amp_values = self.power_spectral_density
-        f_transfer1 = self.frequencies
-        Pf1_interp_func = interp1d(self.frequencies, self.power_spectral_density, bounds_error=False, fill_value=np.inf)
-        Pf1 = Pf1_interp_func(f)
-        if sum(np.isinf(Pf1)) > 0:
-            Pf1[np.isinf(Pf1)] = max(Pf1[~np.isinf(Pf1)])
-        #
-        deltaT = 1./sampling_frequency
+        pf1_interp_func = interp1d(self.frequencies, self.power_spectral_density, bounds_error=False, fill_value=np.inf)
+        pf1 = pf1_interp_func(f)
+        if sum(np.isinf(pf1)) > 0:
+            pf1[np.isinf(pf1)] = max(pf1[~np.isinf(pf1)])
 
-        norm1 = 0.5*(Pf1/deltaF)**0.5
-        re1 = np.random.normal(0,norm1,int(numFreqs))
-        im1 = np.random.normal(0,norm1,int(numFreqs))
-        z1  = re1 + 1j*im1
+        norm1 = 0.5 * (pf1 / delta_f) ** 0.5
+        re1 = np.random.normal(0, norm1, int(number_of_frequencies))
+        im1 = np.random.normal(0, norm1, int(number_of_frequencies))
+        z1 = re1 + 1j * im1
 
         # freq domain solution for htilde1, htilde2 in terms of z1, z2
         htilde1 = z1
         # convolve data with instrument transfer function
-        otilde1 = htilde1*1.
+        otilde1 = htilde1 * 1.
         # set DC and Nyquist = 0
         # python: we are working entirely with positive frequencies
-        if ( np.mod(N,2)==0 ):
+        if np.mod(number_of_samples, 2) == 0:
             otilde1 = np.concatenate(([0], otilde1, [0]))
-            f = np.concatenate(([0], f, [sampling_frequency/2.]))
+            f = np.concatenate(([0], f, [sampling_frequency / 2.]))
         else:
             # no Nyquist frequency when N=odd
             otilde1 = np.concatenate(([0], otilde1))
@@ -136,3 +131,8 @@ class PowerSpectralDensity:
 
         self.frequency_noise_realization = hf
         self.interpolated_frequency = f
+
+    @staticmethod
+    def equally_spaced_frequency_array(deltaF, numFreqs):
+        f = deltaF * np.linspace(1, numFreqs, numFreqs)
+        return f
