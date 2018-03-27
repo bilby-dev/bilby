d2f7a6fe3913edcd1a4845095bfdeb625540f288
Sylvia Biscoveanu <violinasb@yahoo.com>
2018-03-26 23:45:35 -0700

    Add function to convert from ra, dec to theta, phi

--- peyote/utils.py
+++ peyote/utils.py
@@ -9,3 +9,19 @@ def sampling_frequency(time_series):
         raise ValueError("Your time series was not evenly sampled")
     else:
         return 1./(time_series[1] - time_series[0])
+
+
+def ra_dec_to_theta_phi(ra, dec, gmst):
+    '''
+    Convert from RA and DEC to polar coordinates on celestial sphere
+    Input:
+    ra - right ascension in radians
+    dec - declination in radians
+    gmst - Greenwich mean sidereal time of arrival of the signal in radians
+    Output:
+    theta - zenith angle in radians
+    phi - azimuthal angle in radians
+    '''
+    phi = ra - gmst
+    theta = np.pi/2 - dec
+    return theta, phi
