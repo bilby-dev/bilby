60e06f6c83d75ec047ddaad22ca7cc38f939eb26
RorySmith <rory.smith@caltech.edu>
2018-03-29 11:56:43 +1100

    update to likelihood

--- peyote/likelihood.py
+++ peyote/likelihood.py
@@ -4,11 +4,11 @@ class likelihood:
 
 	def __init__(self, detectors):
 
-		self.detector = detectors
+		self.detectors = detectors
 		
 	def logL_cbc(self, source):
 		
-		self.logL = 0 
+		logL = 0 
 
 		waveform_polarizations = source.model(source.params)
 
@@ -18,12 +18,16 @@ class likelihood:
 				
 
 			det_response = detector.response(source.params)
+			time_shift = detector.time_shift(source.params['geocent_time'])
 
 			signal = np.dot(waveform_polarizations, det_response) 
-			
-			self.logL += 4 * detector.deltaF * np.vdot( detector.data - signal, (detector.data - signal) / detector.psd )
+				
+			signal *= np.exp(-1j*2*np.pi*time_shift)
+
+	
+			logL += 4 * detector.deltaF * np.vdot( detector.data - signal, (detector.data - signal) / detector.psd )
 		
-			
+		return logL	
 				
 			
 
