20c9395f3d9192ff90c808d1a30d91f73ebc1d64
plasky <paul.lasky@monash.edu>
2018-03-29 12:44:51 +1100

    we have cosmic explorer noise, too

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -10,7 +10,7 @@ import peyote.detector as det
 from peyote.utils import sampling_frequency
 
 
-time_duration = 100
+time_duration = 1
 time = np.linspace(0, time_duration, 10000)
 fs = sampling_frequency(time)
 
@@ -27,6 +27,7 @@ Create a noise realisation with a default power spectral density
 """
 PSD = det.PowerSpectralDensity()  # instantiate a detector psd
 PSD.import_power_spectral_density()  # import default psd
+#PSD.import_power_spectral_density(spectral_density_file="CE_psd.txt")  # import cosmic explorer
 hf , ff = PSD.noise_realisation(fs, time_duration)
 
 plt.loglog(ff, np.abs(hf))
