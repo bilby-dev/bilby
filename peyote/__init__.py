3937312bc5f6cba157d948df29c20cec6405f537
Nikhil Sarin <nikhil.sarin@ligo.org>
2018-03-29 12:27:45 +1100

    gaussian noise cleaned

--- peyote/__init__.py
+++ peyote/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 
 # import local files
-import utils
+import peyote.utils as utils
 import peyote.detector as detector
 import peyote.prior as prior
 import peyote.parameter as parameter
--- peyote/utils.py
+++ peyote/utils.py
@@ -40,3 +40,45 @@ def gps_time_to_gmst(time):
     gps_time = Time(time, format='gps', scale='utc')
     gmst = gps_time.sidereal_time('mean', 'greenwich').value * np.pi / 12
     return gmst
+
+def create_white_noise(sampling_frequency, duration):
+    '''
+    Create white_noise which is then coloured by a given PSD
+    '''
+
+    number_of_samples = duration * sampling_frequency
+    number_of_samples = int(np.round(number_of_samples))
+
+    # prepare for FFT
+    number_of_frequencies = (number_of_samples-1)//2
+    delta_freq = 1./duration
+
+    f = delta_freq * np.linspace(1, number_of_frequencies, number_of_frequencies)
+
+    norm1 = 0.5*(1./delta_freq)**0.5
+    re1 = np.random.normal(0, norm1, int(number_of_frequencies))
+    im1 = np.random.normal(0, norm1, int(number_of_frequencies))
+    z1 = re1 + 1j*im1
+
+
+    # freq domain solution for htilde1, htilde2 in terms of z1, z2
+    htilde1 = z1
+    # convolve data with instrument transfer function
+    otilde1 = htilde1 * 1.
+    # set DC and Nyquist = 0
+    # python: we are working entirely with positive frequencies
+    if np.mod(number_of_samples, 2) == 0:
+        otilde1 = np.concatenate(([0], otilde1, [0]))
+        f = np.concatenate(([0], f, [sampling_frequency / 2.]))
+    else:
+        # no Nyquist frequency when N=odd
+        otilde1 = np.concatenate(([0], otilde1))
+        f = np.concatenate(([0], f))
+
+    # normalise for positive frequencies and units of strain/rHz
+    white_noise = otilde1
+    # python: transpose for use with infft
+    white_noise = np.transpose(white_noise)
+    f = np.transpose(f)
+
+    return white_noise, f
