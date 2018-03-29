9176105c1dbb29a3b3171ff6228cf87033a34c72
plasky <paul.lasky@monash.edu>
2018-03-29 11:53:48 +1100

    added utils.py to init

--- peyote/__init__.py
+++ peyote/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import print_function, division
 
 # import local files
+import utils
 import peyote.detector as detector
+import peyote.prior as prior
 import peyote.parameter as parameter
 import peyote.source as source
-import peyote.prior as prior
