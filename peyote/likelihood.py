c81e0a60121af8abbb874744bb1b3a3ddc548d81
RorySmith <rory.smith@caltech.edu>
2018-03-29 13:31:05 +1100

    replaced tabs with spaces

--- peyote/likelihood.py
+++ peyote/likelihood.py
@@ -2,41 +2,32 @@ import numpy as np
 
 class likelihood:
 
-	def __init__(self, Interferometers):
+ def __init__(self, Interferometers):
 
-		self.Interferometers = Interferometers
-		
-	def logL_cbc(self, source, params):
-		
-		logL = 0 
+  self.Interferometers = Interferometers
 
-		waveform_polarizations = source.model(params)
+ def logL_cbc(self, source, params):
 
-		
-		for Interferometer in self.Interferometers:
+  logL = 0
+
+  waveform_polarizations = source.model(params)
 
 
-			for mode in source.params['modes'].keys() :
+  for Interferometer in self.Interferometers:
 
-				det_response = Interometer.response(parmams['ra'], parmams['dec'], parmams['psi'], mode)
-				if signal is None:
-					signal = waveform_polarizations['%s'%mode] * det_response
-				else:
-					signal += waveform_polarizations['%s'%mode] * det_response
- 
-			#time_shift = Interferometer.time_shift(source.params['geocent_time'])	
-			#signal *= np.exp(-1j*2*np.pi*time_shift) # This is just here as a reminder that a tc shift needs to be performed 
-			                                          # on frequency-domain GWs					
-			
-			logL += 4 * Interferometer.deltaF * np.vdot( Interferometer.data - signal, (Interferometer.data - signal) / Interferometer.data.psd )
-		
-		return logL	
-				
-			
 
-			
+   for mode in source.params['modes'].keys() :
 
-	
-		
+    det_response = Interometer.response(parmams['ra'], parmams['dec'], parmams['psi'], mode)
+    if signal is None:
+     signal = waveform_polarizations['%s'%mode] * det_response
+    else:
+     signal += waveform_polarizations['%s'%mode] * det_response
 
-		 
+			#time_shift = Interferometer.time_shift(source.params['geocent_time'])
+			#signal *= np.exp(-1j*2*np.pi*time_shift) # This is just here as a reminder that a tc shift needs to be performed
+			                                          # on frequency-domain GWs
+
+   logL += 4 * Interferometer.deltaF * np.vdot( Interferometer.data - signal, (Interferometer.data - signal) / Interferometer.data.psd )
+
+  return logL	
