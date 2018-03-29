fa25619630cb4e29233c2a2649320e87fc8c3a6e
plasky <paul.lasky@monash.edu>
2018-03-29 12:04:35 +1100

    bluh

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -1,5 +1,17 @@
 """
 Basic tutorial to get PEYOte running
 """
+import numpy as np
 
-import peyote
+import peyote.source as src
+import peyote.parameter as par
+
+time = np.linspace(0, 100, 10000)
+
+params = [par.amplitude, par.frequency, par.time_at_coalescence]
+
+foo = src.SimpleSinusoidSource('test', params)
+ht = foo.waveform(time)
+
+plt.plot(time, ht)
+plt.show()
