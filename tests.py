2cc59af8d364b6a6e4750c0196dfaa10f31be119
Gregory Ashton <gregory.ashton@ligo.org>
2018-04-10 09:21:14 +1000

    Update tests
    - Removes previous failed attempt at standard likelihood calculation
    - Adds in a test of recovering the luminosity distance

--- tests.py
+++ tests.py
@@ -44,18 +44,20 @@ class Test(unittest.TestCase):
             all(self.msd['frequencies'] == frequencies_saved))
         self.assertAlmostEqual(all(self.msd['hf_signal_and_noise'] - hf_signal_and_noise_saved), 0.00000000, 5)
 
-    def test_calculate_likelihood_for_standard_data(self):
-        pS = self.msd['simulation_parameters']
-        pN = self.msd['simulation_parameters'].copy()
-        pN['luminosity_distance'] = 1e6
-
+    def test_recover_luminosity_distance(self):
         likelihood = peyote.likelihood.likelihood(
             [self.msd['IFO']], self.msd['source'])
-        loglS = likelihood.loglikelihood(pS)
-        loglN = likelihood.loglikelihood(pN)
-        self.assertAlmostEqual(
-            (loglS - loglN) / 36573752.328521997,
-            1.0000, 3)
+
+        prior = self.msd['simulation_parameters'].copy()
+        dL = self.msd['simulation_parameters']['luminosity_distance']
+        prior['luminosity_distance'] = peyote.parameter.Parameter(
+            'luminosity_distance',
+            prior=peyote.prior.Uniform(lower=dL-10, upper=dL+10))
+
+        result = peyote.run_sampler(likelihood, prior, sampler='nestle',
+                                    verbose=False)
+        self.assertAlmostEqual(np.mean(result.samples), dL,
+                               delta=np.std(result.samples))
 
 
 if __name__ == '__main__':
