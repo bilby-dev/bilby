c683908066339408f435e4d987072365ec153347
moritz <email@moritz-huebner.de>
2018-04-09 17:30:03 +1000

    Moritz Huebner: Moved some dummy initializations of attributes of the Sampler class in its __init__ function.

--- peyote/sampler.py
+++ peyote/sampler.py
@@ -82,6 +82,13 @@ class Sampler:
 
         self.log_summary_for_sampler()
 
+        # Initialize some variables that are going to be used later
+        self.fixed_parameters = []
+        self.result = 0
+        self.search_parameter_keys = []
+        self.ndim = 0
+        self.sampler = 0
+
         if os.path.isdir(outdir) is False:
             os.makedirs(outdir)
 
