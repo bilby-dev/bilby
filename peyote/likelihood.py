12ff0da86c37ddc60d754620968d804e1e41eb38
moritz <email@moritz-huebner.de>
2018-04-10 16:10:53 +1000

    Moritz Huebner: Made the signal_ifo parameter the same as in the remote

--- peyote/likelihood.py
+++ peyote/likelihood.py
@@ -26,8 +26,7 @@ class Likelihood:
             time_shift = interferometer.time_delay_from_geocenter(self.source.ra, self.source.dec,
                                                                   self.source.geocent_time)
 
-            # signal_ifo *= np.exp(1j*2*np.pi*time_shift*self.source.frequency_array)
-            signal_ifo *= np.exp(-1j * 2 * np.pi * time_shift)
+            signal_ifo *= np.exp(1j*2*np.pi*time_shift*self.source.frequency_array)
 
             log_l -= 4. / self.source.time_duration * np.vdot(
                 interferometer.data - signal_ifo,
