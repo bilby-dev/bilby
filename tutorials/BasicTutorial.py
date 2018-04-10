dc6f80f72fe458c22d465adb990256a8f47cb515
moritz <email@moritz-huebner.de>
2018-04-10 18:23:59 +1000

    Moritz Huebner: Uncommented two additional parameters

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -56,12 +56,12 @@ prior = source.copy()
 prior.mass_1 = peyote.parameter.Parameter(
     'mass_1', prior=peyote.prior.Uniform(lower=35, upper=37),
     latex_label='$m_1$')
-#prior.mass_2 = peyote.parameter.Parameter(
-#    'mass_2', prior=peyote.prior.Uniform(lower=27, upper=31),
-#    latex_label='$m_2$')
-#prior.iota = peyote.parameter.Parameter(
-#    'iota', prior=peyote.prior.Uniform(lower=0, upper=np.pi),
-#    latex_label='$iota$')
+prior.mass_2 = peyote.parameter.Parameter(
+    'mass_2', prior=peyote.prior.Uniform(lower=27, upper=31),
+    latex_label='$m_2$')
+prior.iota = peyote.parameter.Parameter(
+    'iota', prior=peyote.prior.Uniform(lower=0, upper=np.pi),
+    latex_label='$iota$')
 prior.luminosity_distance = peyote.parameter.Parameter(
     'luminosity_distance', prior=peyote.prior.Uniform(lower=30, upper=200),
     latex_label='$d_L$')
