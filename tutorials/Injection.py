4d1caf8210702579316e3f47fde87b8f6de78d36
moritz <email@moritz-huebner.de>
2018-04-10 12:37:01 +1000

    Moritz Huebner: Added call to superclass in Dynesty __init__

--- tutorials/Injection.py
+++ tutorials/Injection.py
@@ -64,7 +64,9 @@ prior.mass_1 = peyote.parameter.Parameter('mass_1', prior=peyote.prior.Uniform(l
 prior.mass_2 = peyote.parameter.Parameter('mass_2', prior=peyote.prior.Uniform(lower=28, upper=30),
                                           latex_label='$m_2$')
 
-result = peyote.sampler.run_sampler(likelihood, prior, sampler='dynesty', npoints=100, print_progress=True)
+
+
+# result = peyote.sampler.run_sampler(likelihood, prior, sampler='dynesty', npoints=100, print_progress=True)
 
 print(result.samples)
 
