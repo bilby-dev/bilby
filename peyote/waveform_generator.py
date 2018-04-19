1ee66801615870f9770bb3ec1e1e243c678aa60d
moritz <email@moritz-huebner.de>
2018-04-19 16:17:29 +1000

    Moritz Huebner: Implemented source_model as a property * The parameters attribute will now automatically update if source_model is changed

--- peyote/waveform_generator.py
+++ peyote/waveform_generator.py
@@ -1,7 +1,6 @@
 import inspect
-import copy
-import peyote
 
+from . import utils
 
 class WaveformGenerator(object):
     """ A waveform generator
@@ -27,27 +26,44 @@ class WaveformGenerator(object):
         self.time_duration = time_duration
         self.sampling_frequency = sampling_frequency
         self.source_model = source_model
-        keys = inspect.getargspec(source_model).args
-        keys.pop(0)
-        self.parameters = dict.fromkeys(keys)
+        self.parameters = inspect.getargspec(source_model).args
 
     @property
     def frequency_array(self):
-        return peyote.utils.create_fequency_series(self.sampling_frequency, self.time_duration)
+        return utils.create_fequency_series(self.sampling_frequency, self.time_duration)
 
     @property
     def time_array(self):
-        return peyote.utils.create_time_series(self.sampling_frequency, self.time_duration)
+        return utils.create_time_series(self.sampling_frequency, self.time_duration)
+
+    @property
+    def parameters(self):
+        return self.__parameters
+
+    @property
+    def source_model(self):
+        return self.__source_model
+
+    @parameters.setter
+    def parameters(self, parameters):
+        if isinstance(parameters, list):
+            parameters.pop(0)
+            self.__parameters = dict.fromkeys(parameters)
+        elif isinstance(parameters, dict):
+            for key in self.__parameters.keys():
+                if key in parameters.keys():
+                    self.__parameters[key] = parameters[key]
+                else:
+                    raise KeyError('The provided dictionary did not contain key {}'.format(key))
+        else:
+            raise TypeError('Parameters must either be set as a list of keys or a dictionary of key-value pairs.')
+
+    @source_model.setter
+    def source_model(self, source_model):
+        self.__source_model = source_model
+        self.parameters = inspect.getargspec(source_model).args
 
     def frequency_domain_strain(self):
         """ Wrapper to source_model """
         return self.source_model(self.frequency_array, **self.parameters)
 
-    def set_values(self, dictionary):
-        """ Given a dictionary of values, set the class attributes """
-
-        for key in self.parameters.keys():
-            if key in dictionary.keys():
-                self.parameters[key] = dictionary[key]
-            else:
-                raise KeyError('The provided dictionary did not contain key {}'.format(key))
