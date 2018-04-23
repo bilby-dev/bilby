3d25134351226525d976ccce4f314d7882b32e30
moritz <email@moritz-huebner.de>
2018-04-23 13:21:05 +1000

    Moritz Huebner: Trying to make the CI running again

--- test_runner.py
+++
@@ -1,8 +0,0 @@
-import peyote
-
-exec(open('test/parameter_tests.py').read())
-print('test')
-exec(open('test/tests.py').read())
-print('test')
-exec(open('test/waveform_generator_tests.py').read())
-print('test')
