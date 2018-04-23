f5795919db9eadfde3812e72fcffdd8325f0178e
Colm Talbot <colm.talbot@ligo.org>
2018-04-23 17:10:53 +1000

    update tutorials

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -10,7 +10,7 @@ peyote.utils.setup_logger()
 time_duration = 1.
 sampling_frequency = 4096.
 
-simulation_parameters = dict(
+injection_parameters = dict(
     mass_1=36.,
     mass_2=29.,
     spin11=0,
@@ -29,6 +29,7 @@ simulation_parameters = dict(
     geocent_time=1126259642.413,
     psi=2.659
 )
+simulation_parameters = injection_parameters.copy()
 simulation_parameters = peyote.parameter.Parameter.parse_floats_to_parameters(simulation_parameters)
 # Create the waveform_generator using a LAL BinaryBlackHole source function
 waveform_generator = peyote.waveform_generator.WaveformGenerator(
@@ -50,7 +51,13 @@ L1.set_data(sampling_frequency=sampling_frequency, duration=time_duration,
             from_power_spectral_density=True)
 L1.inject_signal(waveform_polarizations=hf_signal, parameters=simulation_parameters)
 
-IFOs = [H1, L1]
+# Simulate the data in V1
+V1 = peyote.detector.V1
+V1.set_data(sampling_frequency=sampling_frequency, duration=time_duration,
+            from_power_spectral_density=True)
+V1.inject_signal(waveform_polarizations=hf_signal, parameters=simulation_parameters)
+
+IFOs = [H1, L1, V1]
 
 # Plot the noise and signal
 fig, ax = plt.subplots()
@@ -68,8 +75,10 @@ fig.savefig('data')
 likelihood = peyote.likelihood.Likelihood(IFOs, waveform_generator)
 
 # New way way of doing it, still not perfect
-simulation_parameters['mass_1'].prior = peyote.prior.Uniform(lower=35, upper=37)
-simulation_parameters['luminosity_distance'].prior = peyote.prior.Uniform(lower=30, upper=200)
+# simulation_parameters['mass_1'].prior = peyote.prior.Uniform(lower=35, upper=37)
+# simulation_parameters['luminosity_distance'].prior = peyote.prior.Uniform(lower=30, upper=200)
+simulation_parameters["geocent_time"].prior = peyote.prior.Uniform(lower=injection_parameters["geocent_time"]-0.1,
+                                                                   upper=injection_parameters["geocent_time"]+0.1)
 
 result = peyote.sampler.run_sampler(likelihood, sampler='nestle', verbose=True)
 print(result)
