c5d419588aea15d49aa227ecfbf21eefa38202f9
moritz <email@moritz-huebner.de>
2018-04-24 16:26:20 +1000

    Moritz Huebner: Adapted syntax to change in likelihood.py

--- peyote/sampler.py
+++ peyote/sampler.py
@@ -218,7 +218,7 @@ def run_sampler(likelihood, priors, label='label', outdir='outdir',
         sampler = sampler_class(likelihood, priors, sampler, outdir=outdir,
                                 label=label, **sampler_kwargs)
         result = sampler.run_sampler()
-        result.noise_logz = likelihood.noise_log_likelihood
+        result.noise_logz = likelihood.noise_log_likelihood()
         result.log_bayes_factor = result.logz - result.noise_logz
         print("")
         result.save_to_file(outdir=outdir, label=label)
