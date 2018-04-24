b10dbcdf2c3ad6d68d8b4f8a80294f853b60d32e
moritz <email@moritz-huebner.de>
2018-04-24 16:25:46 +1000

    Moritz Huebner: Removed a nonsensical setter

--- peyote/likelihood.py
+++ peyote/likelihood.py
@@ -2,25 +2,16 @@ import numpy as np
 
 
 class Likelihood(object):
-    def __init__(self, interferometers, waveform_generator, noise_log_likelihood=None):
+    def __init__(self, interferometers, waveform_generator):
         self.interferometers = interferometers
         self.waveform_generator = waveform_generator
-        self.noise_log_likelihood = noise_log_likelihood
 
-    @property
     def noise_log_likelihood(self):
-        return self.__noise_log_likelihood
-
-    @noise_log_likelihood.setter
-    def noise_log_likelihood(self, noise_log_likelihood):
-        if noise_log_likelihood is None:
-            log_l = 0
-            for interferometer in self.interferometers:
-                log_l -= 2. / self.waveform_generator.time_duration * np.sum(abs(interferometer.data) ** 2
-                          / interferometer.power_spectral_density_array)
-            self.__noise_log_likelihood = log_l.real
-        else:
-            self.__noise_log_likelihood = noise_log_likelihood
+        log_l = 0
+        for interferometer in self.interferometers:
+            log_l -= 2. / self.waveform_generator.time_duration * np.sum(
+                abs(interferometer.data) ** 2 / interferometer.power_spectral_density_array)
+        return log_l.real
 
     def log_likelihood(self):
         log_l = 0
@@ -38,6 +29,4 @@ class Likelihood(object):
         return log_l.real
 
     def log_likelihood_ratio(self):
-        return self.log_likelihood() - self.noise_log_likelihood
-
-
+        return self.log_likelihood() - self.noise_log_likelihood()
