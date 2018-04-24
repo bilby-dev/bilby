4556c86f26ffdaafbbfab62a0b01a028d73f4d26
moritz <email@moritz-huebner.de>
2018-04-24 15:53:47 +1000

    Moritz Huebner: Deleted obsolete LikelihoodB class

--- peyote/likelihood.py
+++ peyote/likelihood.py
@@ -41,37 +41,3 @@ class Likelihood(object):
         return self.log_likelihood() - self.noise_log_likelihood
 
 
-
-class LikelihoodB(Likelihood):
-
-    def __init__(self, interferometers, waveform_generator):
-        Likelihood.__init__(self, interferometers, waveform_generator)
-
-        for interferometer in self.interferometers:
-            interferometer.whiten_data()
-
-    def log_likelihood(self):
-        log_l = 0
-        waveform_polarizations = self.waveform_generator.frequency_domain_strain()
-        for interferometer in self.interferometers:
-            for mode in waveform_polarizations.keys():
-                det_response = interferometer.antenna_response(
-                    self.waveform_generator.ra, self.waveform_generator.dec,
-                    self.waveform_generator.geocent_time, self.waveform_generator.psi, mode)
-
-                waveform_polarizations[mode] *= det_response
-
-            signal_ifo = np.sum(waveform_polarizations.values(), axis=0)
-
-            time_shift = interferometer.time_delay_from_geocenter(
-                self.waveform_generator.ra, self.waveform_generator.dec,
-                self.waveform_generator.geocent_time)
-            signal_ifo *= np.exp(-1j * 2 * np.pi * time_shift)
-            signal_ifo_whitened = signal_ifo / (
-                interferometer.amplitude_spectral_density_array)
-
-            log_l -= 2. * self.waveform_generator.sampling_frequency * (
-                np.real(sum(
-                    (interferometer.whitened_data - signal_ifo_whitened) ** 2)))
-
-        return log_l
