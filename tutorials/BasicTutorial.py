553a3ad2bbd1499291feb9f57b98c00e32905146
Colm Talbot <colm.talbot@ligo.org>
2018-05-02 15:55:02 +1000

    update BasicTutorial

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -1,32 +1,15 @@
 import numpy as np
 import pylab as plt
-
-import peyote.prior
+import peyote
 
 peyote.utils.setup_logger()
 
 time_duration = 1.
 sampling_frequency = 4096.
 
-injection_parameters = dict(
-    mass_1=36.,
-    mass_2=29.,
-    a_1=0,
-    a_2=0,
-    tilt_1=0,
-    tilt_2=0,
-    phi_1=0,
-    phi_2=0,
-    luminosity_distance=100.,
-    iota=0.4,
-    phase=1.3,
-    waveform_approximant='IMRPhenomPv2',
-    reference_frequency=50.,
-    ra=1.375,
-    dec=-1.2108,
-    geocent_time=1126259642.413,
-    psi=2.659
-)
+injection_parameters = dict(mass_1=36., mass_2=29., a_1=0, a_2=0, tilt_1=0, tilt_2=0, phi_1=0, phi_2=0,
+                            luminosity_distance=2500., iota=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413,
+                            waveform_approximant='IMRPhenomPv2', reference_frequency=50., ra=1.375, dec=-1.2108)
 
 # Create the waveform_generator using a LAL BinaryBlackHole source function
 waveform_generator = peyote.waveform_generator.WaveformGenerator(
@@ -36,56 +19,40 @@ waveform_generator = peyote.waveform_generator.WaveformGenerator(
     parameters=injection_parameters)
 hf_signal = waveform_generator.frequency_domain_strain()
 
-sampling_parameters = peyote.prior.parse_floats_to_fixed_priors(injection_parameters)
+# Set up interferometers.
+IFOs = [peyote.detector.get_empty_interferometer(name) for name in ['H1', 'L1', 'V1']]
 
-#sampling_parameters = peyote.prior.parse_keys_to_parameters(injection_parameters.keys())
-
-
-# Simulate the data in H1
-H1 = peyote.detector.H1
-H1.set_data(sampling_frequency=sampling_frequency, duration=time_duration,
-            from_power_spectral_density=True)
-H1.inject_signal(waveform_polarizations=hf_signal, parameters=injection_parameters)
-
-# Simulate the data in L1
-L1 = peyote.detector.L1
-L1.set_data(sampling_frequency=sampling_frequency, duration=time_duration,
-            from_power_spectral_density=True)
-L1.inject_signal(waveform_polarizations=hf_signal, parameters=injection_parameters)
-
-# Simulate the data in V1
-V1 = peyote.detector.V1
-V1.set_data(sampling_frequency=sampling_frequency, duration=time_duration,
-            from_power_spectral_density=True)
-V1.inject_signal(waveform_polarizations=hf_signal, parameters=injection_parameters)
-
-IFOs = [H1, L1, V1]
+for IFO in IFOs:
+    IFO.set_data(sampling_frequency=sampling_frequency, duration=time_duration, from_power_spectral_density=True)
+    IFO.inject_signal(waveform_polarizations=hf_signal, parameters=injection_parameters)
 
 # Plot the noise and signal
 fig, ax = plt.subplots()
-plt.loglog(H1.frequency_array, np.abs(H1.data), lw=1.5, label='H1 noise+signal')
-plt.loglog(H1.frequency_array, H1.amplitude_spectral_density_array, lw=1.5, label='H1 ASD')
-plt.loglog(L1.frequency_array, np.abs(L1.data), lw=1.5, label='L1 noise+signal')
-plt.loglog(L1.frequency_array, L1.amplitude_spectral_density_array, lw=1.5, label='H1 ASD')
-# plt.loglog(frequencies, np.abs(hf_signal['plus']), lw=0.8, label='signal')
+for IFO in IFOs:
+    plt.loglog(IFO.frequency_array, np.abs(IFO.data), lw=1.5, label='{} noise+signal'.format(IFO.name))
+    plt.loglog(IFO.frequency_array, IFO.amplitude_spectral_density_array, lw=1.5, label='{} ASD'.format(IFO.name))
 plt.xlim(10, 1000)
 plt.legend()
 plt.xlabel(r'frequency')
 plt.ylabel(r'strain')
 fig.savefig('data')
 
+# Set up likelihood
 likelihood = peyote.likelihood.Likelihood(IFOs, waveform_generator)
 
-# New way way of doing it, still not perfect
-sampling_parameters['mass_1'] = peyote.prior.Uniform(
+# Set up prior
+priors = {}
+priors['mass_1'] = peyote.prior.Uniform(
     lower=35, upper=37, name='mass_1', latex_label='$m_1$')
-sampling_parameters['luminosity_distance'] = peyote.prior.Uniform(lower=30, upper=200, name='luminosity_distance')
-#sampling_parameters["geocent_time"].prior = peyote.prior.Uniform(lower=injection_parameters["geocent_time"] - 0.1,
-#                                                                  upper=injection_parameters["geocent_time"]+0.1)
+priors['luminosity_distance'] = peyote.prior.create_default_prior('luminosity_distance')
+for key in ['mass_2', 'a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_1', 'phi_2', 'phase', 'psi', 'iota', 'ra',
+            'dec', 'geocent_time']:
+    priors[key] = injection_parameters[key]
 
-result = peyote.sampler.run_sampler(
-    likelihood, priors=sampling_parameters, label='BasicTutorial',
-    sampler='nestle', verbose=True, injection_parameters=injection_parameters)
+# Run sampler
+result = peyote.sampler.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty',
+                                    label='BasicTutorial', use_ratio=True, npoints=500, verbose=True,
+                                    injection_parameters=injection_parameters)
 result.plot_corner()
 result.plot_walks()
 result.plot_distributions()
