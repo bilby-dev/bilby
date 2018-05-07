24051fac11bec9a5cf616a8e0bba18fc6f789baf
Colm Talbot <colm.talbot@ligo.org>
2018-05-07 11:40:09 +1000

    activate time marginalisation

--- tutorials/BasicTutorial.py
+++ tutorials/BasicTutorial.py
@@ -2,16 +2,16 @@ import numpy as np
 import pylab as plt
 import peyote
 
-peyote.utils.setup_logger()
+peyote.utils.setup_logger(log_level="info")
 
 time_duration = 4.
 sampling_frequency = 2048.
 outdir = 'outdir'
 
-np.random.seed(170809)
+np.random.seed(170814)
 
 injection_parameters = dict(mass_1=36., mass_2=29., a_1=0.4, a_2=0, tilt_1=0, tilt_2=0, phi_12=0, phi_jl=0,
-                            luminosity_distance=8000., iota=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413,
+                            luminosity_distance=400., iota=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413,
                             waveform_approximant='IMRPhenomPv2', reference_frequency=50., ra=1.375, dec=-1.2108)
 
 # Create the waveform_generator using a LAL BinaryBlackHole source function
@@ -33,13 +33,13 @@ priors = dict()
 for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl', 'ra', 'dec', 'phase', 'geocent_time',
             'mass_1', 'mass_2']:
     priors[key] = injection_parameters[key]
-priors['luminosity_distance'] = peyote.prior.PowerLaw(alpha=2, minimum=500, maximum=10000, name='luminosity_distance')
-# priors['phase'] = peyote.prior.create_default_prior(name='phase')
+priors['luminosity_distance'] = peyote.prior.PowerLaw(alpha=2, minimum=100, maximum=10000, name='luminosity_distance')
 # priors['mass_1'] = peyote.prior.Uniform(30, 50, name='mass_1')
 
 # Set up likelihood
 likelihood = peyote.likelihood.MarginalizedLikelihood(IFOs, waveform_generator, prior=priors,
-                                                      distance_marginalization=False, phase_marginalization=False)
+                                                      distance_marginalization=False, phase_marginalization=True,
+                                                      time_marginalization=True)
 # likelihood = peyote.likelihood.Likelihood(IFOs, waveform_generator)
 
 # Run sampler
