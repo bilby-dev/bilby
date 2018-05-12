2df8bc2cabf06f29b1d1c4b933fb66d670a91608
Colm Talbot <colm.talbot@ligo.org>
2018-05-12 12:48:19 +1000

    update syntax in example

--- examples/injection_examples/change_sampled_parameters.py
+++ examples/injection_examples/change_sampled_parameters.py
@@ -21,20 +21,13 @@ injection_parameters = dict(mass_1=36., mass_2=29., a_1=0.4, a_2=0.3, tilt_1=0.5
                             luminosity_distance=4000., iota=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413,
                             waveform_approximant='IMRPhenomPv2', reference_frequency=50., ra=1.375, dec=-1.2108)
 
-# injection_parameters = dict(chirp_mass=25., mass_ratio=0.8, a_1=0.4, a_2=0.3, tilt_1=0.5, tilt_2=1.0, phi_12=1.7, phi_jl=0.3,
-#                             luminosity_distance=4000., iota=0.4, psi=2.659, phase=1.3, geocent_time=1126259642.413,
-#                             waveform_approximant='IMRPhenomPv2', reference_frequency=50., ra=1.375, dec=-1.2108)
-
 # Create the waveform_generator using a LAL BinaryBlackHole source function
 waveform_generator = tupak.waveform_generator.WaveformGenerator(
     sampling_frequency=sampling_frequency, time_duration=time_duration,
     frequency_domain_source_model=tupak.source.lal_binary_black_hole,
-    parameter_conversion=tupak.source.convert_to_lal_binary_black_hole_parameters,
+    parameter_conversion=tupak.conversion.convert_to_lal_binary_black_hole_parameters,
     parameters=injection_parameters)
 hf_signal = waveform_generator.frequency_domain_strain()
-
-tupak.source.convert_from_lal_binary_black_hole_parameters(parameters=injection_parameters,
-                                                           search_keys=['chirp_mass', 'mass_ratio'])
 
 # Set up interferometers.
 IFOs = [tupak.detector.get_interferometer_with_fake_noise_and_injection(
@@ -47,16 +40,14 @@ priors = dict()
 for key in ['a_1', 'a_2', 'tilt_1', 'tilt_2', 'phi_12', 'phi_jl', 'phase', 'psi', 'iota', 'ra', 'dec', 'geocent_time']:
     priors[key] = injection_parameters[key]
 priors['luminosity_distance'] = tupak.prior.create_default_prior(name='luminosity_distance')
-priors['chirp_mass'] = tupak.prior.create_default_prior('chirp_mass')
-priors['mass_ratio'] = tupak.prior.create_default_prior('mass_ratio')
 
 # Initialise Likelihood
 likelihood = tupak.likelihood.Likelihood(interferometers=IFOs, waveform_generator=waveform_generator)
 
 # Run sampler
 result = tupak.sampler.run_sampler(likelihood=likelihood, priors=priors, sampler='dynesty', npoints=100, walks=10,
-                                   injection_parameters=injection_parameters, outdir=outdir,
-                                   label='DifferentParameters')
+                                   injection_parameters=injection_parameters, label='DifferentParameters',
+                                   sampling_parameters=['chirp_mass', 'mass_ratio', 'cos_iota'], outdir=outdir)
 result.plot_corner()
 result.plot_walks()
 result.plot_distributions()
