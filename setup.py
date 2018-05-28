eb891603f9062e55809d800c0e1ee20df044a9a1
Gregory Ashton <gregory.ashton@ligo.org>
2018-05-28 15:29:58 +1000

    Fix bug when installing from tar

--- setup.py
+++ setup.py
@@ -4,16 +4,23 @@ from distutils.core import setup
 import subprocess
 
 # Write a version file containing the git hash and info
-git_log = subprocess.check_output(['git', 'log', '-1', '--pretty=%h %ai']).decode('utf-8')
-git_diff = (subprocess.check_output(['git', 'diff', '.'])
-            + subprocess.check_output(['git', 'diff', '--cached', '.'])).decode('utf-8')
-if git_diff == '':
-    status = '(CLEAN) ' + git_log
-else:
-    status = '(UNCLEAN) ' + git_log
+try:
+    git_log = subprocess.check_output(
+        ['git', 'log', '-1', '--pretty=%h %ai']).decode('utf-8')
+    git_diff = (subprocess.check_output(['git', 'diff', '.'])
+                + subprocess.check_output(
+                    ['git', 'diff', '--cached', '.'])).decode('utf-8')
+    if git_diff == '':
+        status = '(CLEAN) ' + git_log
+    else:
+        status = '(UNCLEAN) ' + git_log
+except subprocess.CalledProcessError:
+    status = "NO VERSION INFORMATION"
+
 version_file = '.version'
 with open('tupak/' + version_file, 'w+') as f:
     f.write(status)
+
 
 setup(name='tupak',
       version='0.1',
