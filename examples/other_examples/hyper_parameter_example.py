2f6d63e9e150f7f5d23141ffa755b672f9bf518f
Gregory Ashton <gregory.ashton@ligo.org>
2018-06-08 10:21:02 +1000

    Update hyper parameter example and add documentation

--- examples/other_examples/hyper_parameter_example.py
+++ examples/other_examples/hyper_parameter_example.py
@@ -1,82 +1,122 @@
 #!/bin/python
 """
-An example of how to use tupak to perform paramater estimation for hyperparams
+An example of how to use tupak to perform paramater estimation for hyper params
 """
 from __future__ import division
 import tupak
 import numpy as np
+import inspect
+import matplotlib.pyplot as plt
 
 tupak.core.utils.setup_logger()
 outdir = 'outdir'
 
 
-class GaussianLikelihood(tupak.core.likelihood.Likelihood):
-    def __init__(self, x, y, waveform_generator):
+class GaussianLikelihood(tupak.Likelihood):
+    def __init__(self, x, y, function, sigma=None):
+        """
+        A general Gaussian likelihood for known or unknown noise - the model
+        parameters are inferred from the arguments of function
+
+        Parameters
+        ----------
+        x, y: array_like
+            The data to analyse
+        function:
+            The python function to fit to the data. Note, this must take the
+            dependent variable as its first argument. The other arguments are
+            will require a prior and will be sampled over (unless a fixed
+            value is given).
+        sigma: None, float, array_like
+            If None, the standard deviation of the noise is unknown and will be
+            estimated (note: this requires a prior to be given for sigma). If
+            not None, this defined the standard-deviation of the data points.
+            This can either be a single float, or an array with length equal
+            to that for `x` and `y`.
+        """
         self.x = x
         self.y = y
         self.N = len(x)
-        self.waveform_generator = waveform_generator
-        self.parameters = waveform_generator.parameters
+        self.sigma = sigma
+        self.function = function
+
+        # These lines of code infer the parameters from the provided function
+        parameters = inspect.getargspec(function).args
+        parameters.pop(0)
+        self.parameters = dict.fromkeys(parameters)
+        self.function_keys = self.parameters.keys()
+        if self.sigma is None:
+            self.parameters['sigma'] = None
 
     def log_likelihood(self):
-        sigma = 1
-        res = self.y - self.waveform_generator.time_domain_strain()
+        sigma = self.parameters.get('sigma', self.sigma)
+        model_parameters = {k: self.parameters[k] for k in self.function_keys}
+        res = self.y - self.function(self.x, **model_parameters)
         return -0.5 * (np.sum((res / sigma)**2)
                        + self.N*np.log(2*np.pi*sigma**2))
 
 
-def model(time, m):
-    return m * time
+# Define a model to fit to each data set
+def model(x, c0, c1):
+    return c0 + c1*x
 
 
-sampling_frequency = 10
-time_duration = 100
-time = np.arange(0, time_duration, 1/sampling_frequency)
+N = 10
+x = np.linspace(0, 10, N)
+sigma = 1
+Nevents = 3
+labels = ['a', 'b', 'c']
 
-true_mu_m = 5
-true_sigma_m = 0.1
-sigma = 0.1
-Nevents = 10
-samples = []
+true_mu_c0 = 5
+true_sigma_c0 = 1
+
+fig1, ax1 = plt.subplots()
+fig2, ax2 = plt.subplots()
 
 # Make the sample sets
+samples = []
 for i in range(Nevents):
-    m = np.random.normal(true_mu_m, true_sigma_m)
-    injection_parameters = dict(m=m)
+    c0 = np.random.normal(true_mu_c0, true_sigma_c0)
+    c1 = np.random.uniform(-1, 1)
+    injection_parameters = dict(c0=c0, c1=c1)
 
-    N = len(time)
-    data = model(time, **injection_parameters) + np.random.normal(0, sigma, N)
+    data = model(x, **injection_parameters) + np.random.normal(0, sigma, N)
+    line = ax1.plot(x, data, '-x', label=labels[i])
 
-    waveform_generator = tupak.gw.waveform_generator.WaveformGenerator(
-        time_duration=time_duration, sampling_frequency=sampling_frequency,
-        time_domain_source_model=model)
-
-    likelihood = GaussianLikelihood(time, data, waveform_generator)
-
-    priors = dict(m=tupak.core.prior.Uniform(-10, 10, 'm'))
+    likelihood = GaussianLikelihood(x, data, model, sigma)
+    priors = dict(c0=tupak.core.prior.Uniform(-10, 10, 'c0'),
+                  c1=tupak.core.prior.Uniform(-10, 10, 'c1'),
+                  )
 
     result = tupak.core.sampler.run_sampler(
         likelihood=likelihood, priors=priors, sampler='dynesty', npoints=1000,
-        injection_parameters=injection_parameters, outdir=outdir,
-        verbose=False, label='individual_{}'.format(i), use_ratio=False,
-        sample='unif')
-    result.plot_corner()
-    samples.append(result.samples)
+        outdir=outdir, verbose=False, label='individual_{}'.format(i))
+    ax2.hist(result.posterior.c0, color=line[0].get_color(), normed=True,
+             alpha=0.5, label=labels[i])
+    samples.append(result.posterior.c0.values)
 
-# Now run the hyperparameter inference
-run_prior = tupak.core.prior.Uniform(minimum=-10, maximum=10, name='mu_m')
+ax1.set_xlabel('x')
+ax1.set_ylabel('y(x)')
+ax1.legend()
+fig1.savefig('outdir/hyper_parameter_data.png')
+ax2.set_xlabel('c0')
+ax2.set_ylabel('density')
+ax2.legend()
+fig2.savefig('outdir/hyper_parameter_combined_posteriors.png')
+
+# Now run the hyper parameter inference
+run_prior = tupak.core.prior.Uniform(minimum=-10, maximum=10, name='mu_c0')
 hyper_prior = tupak.core.prior.Gaussian(mu=0, sigma=1, name='hyper')
 
 hp_likelihood = tupak.gw.likelihood.HyperparameterLikelihood(
         samples, hyper_prior, run_prior)
 
 hp_priors = dict(
-    mu=tupak.core.prior.Uniform(-10, 10, 'mu', '$\mu_m$'),
-    sigma=tupak.core.prior.Uniform(0, 10, 'sigma', '$\sigma_m$'))
+    mu=tupak.core.prior.Uniform(-10, 10, 'mu', '$\mu_{c0}$'),
+    sigma=tupak.core.prior.Uniform(0, 10, 'sigma', '$\sigma_{c0}$'))
 
 # And run sampler
 result = tupak.core.sampler.run_sampler(
     likelihood=hp_likelihood, priors=hp_priors, sampler='dynesty',
-    npoints=1000, outdir=outdir, label='hyperparameter', use_ratio=False,
-    sample='unif', verbose=True)
-result.plot_corner(truth=dict(mu=true_mu_m, sigma=true_sigma_m))
+    npoints=1000, outdir=outdir, label='hyper_parameter', verbose=True)
+result.plot_corner(truth=dict(mu=true_mu_c0, sigma=true_sigma_c0))
