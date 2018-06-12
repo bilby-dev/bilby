b160084e949e263c79717f1cdf5ede7f8e85fff6
Moritz Huebner <email@moritz-huebner.de>
2018-06-12 11:57:37 +1000

    Created stubs

--- tupak/core/likelihood.py
+++ tupak/core/likelihood.py
@@ -12,15 +12,31 @@ class Likelihood(object):
     """ Empty likelihood class to be subclassed by other likelihoods """
 
     def __init__(self, parameters=None):
+        """
+
+        :param parameters:
+        """
         self.parameters = parameters
 
     def log_likelihood(self):
+        """
+
+        :return:
+        """
         return np.nan
 
     def noise_log_likelihood(self):
+        """
+
+        :return:
+        """
         return np.nan
 
     def log_likelihood_ratio(self):
+        """
+
+        :return:
+        """
         return self.log_likelihood() - self.noise_log_likelihood()
 
 
