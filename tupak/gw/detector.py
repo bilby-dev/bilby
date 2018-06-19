3ae8af102ce8ec47da6b63f1e71bfaa88d1fce26
Gregory Ashton <gregory.ashton@ligo.org>
2018-06-19 13:32:44 +1000

    Merge in changes in 08ae8cfa into fork

--- tupak/gw/detector.py
+++ tupak/gw/detector.py
@@ -833,7 +833,7 @@ class PowerSpectralDensity:
                 logging.warning("You may have intended to provide this as a power spectral density.")
         elif frame_file is not None:
             strain = tupak.gw.utils.read_frame_file(frame_file, t1=start_time - psd_duration - psd_offset,
-                                                    t2=start_time - psd_duration, channel=channel_name)
+                                                    t2=start_time - psd_offset, channel=channel_name)
             sampling_frequency = int(strain.sample_rate.value)
 
             # Low pass filter
