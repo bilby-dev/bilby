57ec4f9ef602925fd1a633779d90c01eea8c32bd
Colm Talbot <colm.talbot@ligo.org>
2018-06-20 21:25:39 +1000

    reinstate check on array length

--- tupak/gw/detector.py
+++ tupak/gw/detector.py
@@ -684,14 +684,14 @@ class PowerSpectralDensity(object):
 
     @power_spectral_density.setter
     def power_spectral_density(self, power_spectral_density):
-        # if self._check_frequency_array_matches_density_array(power_spectral_density):
-        self.__power_spectral_density = power_spectral_density
-        self._interpolate_power_spectral_density()
+        if self._check_frequency_array_matches_density_array(power_spectral_density):
+            self.__power_spectral_density = power_spectral_density
+            self._interpolate_power_spectral_density()
         # print(self.__both_updated)
         # self.__both_updated = ~self.__both_updated
         # print(self.__both_updated)
         # if not self.__both_updated:
-        self.__amplitude_spectral_density = power_spectral_density**0.5
+            self.__amplitude_spectral_density = power_spectral_density**0.5
 
     @property
     def amplitude_spectral_density(self):
@@ -699,14 +699,14 @@ class PowerSpectralDensity(object):
 
     @amplitude_spectral_density.setter
     def amplitude_spectral_density(self, amplitude_spectral_density):
-        # if self._check_frequency_array_matches_density_array(amplitude_spectral_density):
-        self.__amplitude_spectral_density = amplitude_spectral_density
+        if self._check_frequency_array_matches_density_array(amplitude_spectral_density):
+            self.__amplitude_spectral_density = amplitude_spectral_density
         # print(self.__both_updated)
         # self.__both_updated = ~self.__both_updated
         # print(self.__both_updated)
         # if not self.__both_updated:
-        self.__power_spectral_density = amplitude_spectral_density**2
-        self._interpolate_power_spectral_density()
+            self.__power_spectral_density = amplitude_spectral_density**2
+            self._interpolate_power_spectral_density()
 
     def import_amplitude_spectral_density(self):
         """
