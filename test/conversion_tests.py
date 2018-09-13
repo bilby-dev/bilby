46b9e61a5b7048b91460ab3fb47110ec39da6e5c
Moritz Huebner <moritz.huebner@ligo.org>
2018-09-12 22:09:38 -0500

    Revert "Merge branch '176-add-neutron-star-merger-to-examples' into 'master'"
    This reverts merge request !171

--- test/conversion_tests.py
+++ test/conversion_tests.py
@@ -8,35 +8,14 @@ import numpy as np
 class TestBasicConversions(unittest.TestCase):
 
     def setUp(self):
-        self.mass_1 = 1.4
-        self.mass_2 = 1.3
-        self.mass_ratio = 13/14
-        self.total_mass = 2.7
-        self.chirp_mass = (1.4 * 1.3)**0.6 / 2.7**0.2
-        self.symmetric_mass_ratio = (1.4 * 1.3) / 2.7**2
+        self.mass_1 = 20
+        self.mass_2 = 10
+        self.mass_ratio = 0.5
+        self.total_mass = 30
+        self.chirp_mass = 200**0.6 / 30**0.2
+        self.symmetric_mass_ratio = 2/9
         self.cos_angle = -1
         self.angle = np.pi
-        self.lambda_1 = 300
-        self.lambda_2 = 300 * (14 / 13)**5
-        self.lambda_tilde = 8 / 13 * (
-            (1 + 7 * self.symmetric_mass_ratio
-             - 31 * self.symmetric_mass_ratio**2)
-            * (self.lambda_1 + self.lambda_2)
-            + (1 - 4 * self.symmetric_mass_ratio)**0.5
-            * (1 + 9 * self.symmetric_mass_ratio
-               - 11 * self.symmetric_mass_ratio**2)
-            * (self.lambda_1 - self.lambda_2)
-        )
-        self.delta_lambda = 1 / 2 * (
-                (1 - 4 * self.symmetric_mass_ratio)**0.5
-                * (1 - 13272 / 1319 * self.symmetric_mass_ratio
-                   + 8944 / 1319 * self.symmetric_mass_ratio**2)
-                * (self.lambda_1 + self.lambda_2)
-                + (1 - 15910 / 1319 * self.symmetric_mass_ratio
-                   + 32850 / 1319 * self.symmetric_mass_ratio**2
-                   + 3380 / 1319 * self.symmetric_mass_ratio**3)
-                * (self.lambda_1 - self.lambda_2)
-        )
 
     def tearDown(self):
         del self.mass_1
@@ -48,8 +27,7 @@ class TestBasicConversions(unittest.TestCase):
 
     def test_total_mass_and_mass_ratio_to_component_masses(self):
         mass_1, mass_2 = tupak.gw.conversion.total_mass_and_mass_ratio_to_component_masses(self.mass_ratio, self.total_mass)
-        self.assertTrue(all([abs(mass_1 - self.mass_1) < 1e-5,
-                             abs(mass_2 - self.mass_2) < 1e-5]))
+        self.assertTupleEqual((mass_1, mass_2), (self.mass_1, self.mass_2))
 
     def test_symmetric_mass_ratio_to_mass_ratio(self):
         mass_ratio = tupak.gw.conversion.symmetric_mass_ratio_to_mass_ratio(self.symmetric_mass_ratio)
@@ -82,20 +60,6 @@ class TestBasicConversions(unittest.TestCase):
     def test_mass_1_and_chirp_mass_to_mass_ratio(self):
         mass_ratio = tupak.gw.conversion.mass_1_and_chirp_mass_to_mass_ratio(self.mass_1, self.chirp_mass)
         self.assertAlmostEqual(self.mass_ratio, mass_ratio)
-
-    def test_lambda_tilde_to_lambda_1_lambda_2(self):
-        lambda_1, lambda_2 =\
-            tupak.gw.conversion.lambda_tilde_to_lambda_1_lambda_2(
-                self.lambda_tilde, self.mass_1, self.mass_2)
-        self.assertTrue(all([abs(self.lambda_1 - lambda_1) < 1e-5,
-                             abs(self.lambda_2 - lambda_2) < 1e-5]))
-
-    def test_lambda_tilde_delta_lambda_to_lambda_1_lambda_2(self):
-        lambda_1, lambda_2 =\
-            tupak.gw.conversion.lambda_tilde_delta_lambda_to_lambda_1_lambda_2(
-                self.lambda_tilde, self.delta_lambda, self.mass_1, self.mass_2)
-        self.assertTrue(all([abs(self.lambda_1 - lambda_1) < 1e-5,
-                             abs(self.lambda_2 - lambda_2) < 1e-5]))
 
 
 class TestConvertToLALBBHParams(unittest.TestCase):
