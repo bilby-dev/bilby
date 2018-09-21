98d53d947cfd12ecd31b6f76985b4df366b15772
Colm Talbot <colm.talbot@ligo.org>
2018-09-21 12:12:23 +1000

    fix waveform generator tests

--- test/waveform_generator_test.py
+++ test/waveform_generator_test.py
@@ -35,14 +35,13 @@ class TestWaveformGeneratorInstantiationWithoutOptionalParameters(unittest.TestC
 
     def test_repr(self):
         expected = 'WaveformGenerator(duration={}, sampling_frequency={}, start_time={}, ' \
-                   'frequency_domain_source_model={}, time_domain_source_model={}, parameters={}, ' \
+                   'frequency_domain_source_model={}, time_domain_source_model={}, ' \
                    'parameter_conversion={}, waveform_arguments={})'\
             .format(self.waveform_generator.duration,
                     self.waveform_generator.sampling_frequency,
                     self.waveform_generator.start_time,
                     self.waveform_generator.frequency_domain_source_model.__name__,
                     self.waveform_generator.time_domain_source_model,
-                    self.waveform_generator.parameters,
                     None,
                     self.waveform_generator.waveform_arguments)
         self.assertEqual(expected, repr(self.waveform_generator))
@@ -63,6 +62,7 @@ class TestWaveformGeneratorInstantiationWithoutOptionalParameters(unittest.TestC
         self.assertIsInstance(self.waveform_generator.time_array, np.ndarray)
 
     def test_source_model_parameters(self):
+        self.waveform_generator.parameters = self.simulation_parameters.copy()
         self.assertListEqual(sorted(list(self.waveform_generator.parameters.keys())),
                              sorted(list(self.simulation_parameters.keys())))
 
@@ -99,14 +99,15 @@ class TestSetters(unittest.TestCase):
         del self.simulation_parameters
 
     def test_parameter_setter_sets_expected_values_with_expected_keys(self):
-        self.waveform_generator.parameters = self.simulation_parameters
+        self.waveform_generator.parameters = self.simulation_parameters.copy()
         for key in self.simulation_parameters:
             self.assertEqual(self.waveform_generator.parameters[key], self.simulation_parameters[key])
 
     def test_parameter_setter_none_handling(self):
-        self.waveform_generator.parameters = None
-        self.assertListEqual(sorted(list(self.waveform_generator.parameters.keys())),
-                             sorted(list(self.simulation_parameters.keys())))
+        with self.assertRaises(TypeError):
+            self.waveform_generator.parameters = None
+        # self.assertListEqual(sorted(list(self.waveform_generator.parameters.keys())),
+        #                      sorted(list(self.simulation_parameters.keys())))
 
     def test_frequency_array_setter(self):
         new_frequency_array = np.arange(1, 100)
@@ -120,11 +121,13 @@ class TestSetters(unittest.TestCase):
 
     def test_parameters_set_from_frequency_domain_source_model(self):
         self.waveform_generator.frequency_domain_source_model = dummy_func_dict_return_value
+        self.waveform_generator.parameters = self.simulation_parameters.copy()
         self.assertListEqual(sorted(list(self.waveform_generator.parameters.keys())),
                              sorted(list(self.simulation_parameters.keys())))
 
     def test_parameters_set_from_time_domain_source_model(self):
         self.waveform_generator.time_domain_source_model = dummy_func_dict_return_value
+        self.waveform_generator.parameters = self.simulation_parameters.copy()
         self.assertListEqual(sorted(list(self.waveform_generator.parameters.keys())),
                              sorted(list(self.simulation_parameters.keys())))
 
@@ -148,10 +151,10 @@ class TestFrequencyDomainStrainMethod(unittest.TestCase):
     def test_parameter_conversion_is_called(self):
         self.waveform_generator.parameter_conversion = MagicMock(side_effect=KeyError('test'))
         with self.assertRaises(KeyError):
-            self.waveform_generator.frequency_domain_strain()
+            self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
 
     def test_frequency_domain_source_model_call(self):
-        self.waveform_generator.parameters = self.simulation_parameters
         expected = self.waveform_generator.frequency_domain_source_model(self.waveform_generator.frequency_array,
                                                                          self.simulation_parameters['amplitude'],
                                                                          self.simulation_parameters['mu'],
@@ -160,36 +163,39 @@ class TestFrequencyDomainStrainMethod(unittest.TestCase):
                                                                          self.simulation_parameters['dec'],
                                                                          self.simulation_parameters['geocent_time'],
                                                                          self.simulation_parameters['psi'])
-        actual = self.waveform_generator.frequency_domain_strain()
+        actual = self.waveform_generator.frequency_domain_strain(
+            parameters=self.simulation_parameters)
         self.assertTrue(np.array_equal(expected['plus'], actual['plus']))
         self.assertTrue(np.array_equal(expected['cross'], actual['cross']))
 
     def test_time_domain_source_model_call_with_ndarray(self):
         self.waveform_generator.frequency_domain_source_model = None
         self.waveform_generator.time_domain_source_model = dummy_func_array_return_value
-        self.waveform_generator.parameters = self.simulation_parameters
 
         def side_effect(value, value2):
             return value
 
         with mock.patch('tupak.core.utils.nfft') as m:
             m.side_effect = side_effect
-            expected = self.waveform_generator.time_domain_strain()
-            actual = self.waveform_generator.frequency_domain_strain()
+            expected = self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
+            actual = self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
             self.assertTrue(np.array_equal(expected, actual))
 
     def test_time_domain_source_model_call_with_dict(self):
         self.waveform_generator.frequency_domain_source_model = None
         self.waveform_generator.time_domain_source_model = dummy_func_dict_return_value
-        self.waveform_generator.parameters = self.simulation_parameters
 
         def side_effect(value, value2):
             return value, self.waveform_generator.frequency_array
 
         with mock.patch('tupak.core.utils.nfft') as m:
             m.side_effect = side_effect
-            expected = self.waveform_generator.time_domain_strain()
-            actual = self.waveform_generator.frequency_domain_strain()
+            expected = self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
+            actual = self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
             self.assertTrue(np.array_equal(expected['plus'], actual['plus']))
             self.assertTrue(np.array_equal(expected['cross'], actual['cross']))
 
@@ -197,7 +203,8 @@ class TestFrequencyDomainStrainMethod(unittest.TestCase):
         self.waveform_generator.time_domain_source_model = None
         self.waveform_generator.frequency_domain_source_model = None
         with self.assertRaises(RuntimeError):
-            self.waveform_generator.frequency_domain_strain()
+            self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
 
     def test_key_popping(self):
         self.waveform_generator.parameter_conversion = MagicMock(return_value=(dict(amplitude=1e-21, mu=100, sigma=1,
@@ -206,7 +213,8 @@ class TestFrequencyDomainStrainMethod(unittest.TestCase):
                                                                                     psi=2.659, c=None, d=None),
                                                                                ['c', 'd']))
         try:
-            self.waveform_generator.frequency_domain_strain()
+            self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
         except RuntimeError:
             pass
         self.assertListEqual(sorted(self.waveform_generator.parameters.keys()),
@@ -232,10 +240,10 @@ class TestTimeDomainStrainMethod(unittest.TestCase):
     def test_parameter_conversion_is_called(self):
         self.waveform_generator.parameter_conversion = MagicMock(side_effect=KeyError('test'))
         with self.assertRaises(KeyError):
-            self.waveform_generator.time_domain_strain()
+            self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
 
     def test_time_domain_source_model_call(self):
-        self.waveform_generator.parameters = self.simulation_parameters
         expected = self.waveform_generator.time_domain_source_model(self.waveform_generator.time_array,
                                                                     self.simulation_parameters['amplitude'],
                                                                     self.simulation_parameters['mu'],
@@ -244,36 +252,39 @@ class TestTimeDomainStrainMethod(unittest.TestCase):
                                                                     self.simulation_parameters['dec'],
                                                                     self.simulation_parameters['geocent_time'],
                                                                     self.simulation_parameters['psi'])
-        actual = self.waveform_generator.time_domain_strain()
+        actual = self.waveform_generator.time_domain_strain(
+            parameters=self.simulation_parameters)
         self.assertTrue(np.array_equal(expected['plus'], actual['plus']))
         self.assertTrue(np.array_equal(expected['cross'], actual['cross']))
 
     def test_frequency_domain_source_model_call_with_ndarray(self):
         self.waveform_generator.time_domain_source_model = None
         self.waveform_generator.frequency_domain_source_model = dummy_func_array_return_value
-        self.waveform_generator.parameters = self.simulation_parameters
 
         def side_effect(value, value2):
             return value
 
         with mock.patch('tupak.core.utils.infft') as m:
             m.side_effect = side_effect
-            expected = self.waveform_generator.frequency_domain_strain()
-            actual = self.waveform_generator.time_domain_strain()
+            expected = self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
+            actual = self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
             self.assertTrue(np.array_equal(expected, actual))
 
     def test_frequency_domain_source_model_call_with_dict(self):
         self.waveform_generator.time_domain_source_model = None
         self.waveform_generator.frequency_domain_source_model = dummy_func_dict_return_value
-        self.waveform_generator.parameters = self.simulation_parameters
 
         def side_effect(value, value2):
             return value
 
         with mock.patch('tupak.core.utils.infft') as m:
             m.side_effect = side_effect
-            expected = self.waveform_generator.frequency_domain_strain()
-            actual = self.waveform_generator.time_domain_strain()
+            expected = self.waveform_generator.frequency_domain_strain(
+                parameters=self.simulation_parameters)
+            actual = self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
             self.assertTrue(np.array_equal(expected['plus'], actual['plus']))
             self.assertTrue(np.array_equal(expected['cross'], actual['cross']))
 
@@ -281,7 +292,8 @@ class TestTimeDomainStrainMethod(unittest.TestCase):
         self.waveform_generator.time_domain_source_model = None
         self.waveform_generator.frequency_domain_source_model = None
         with self.assertRaises(RuntimeError):
-            self.waveform_generator.time_domain_strain()
+            self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
 
     def test_key_popping(self):
         self.waveform_generator.parameter_conversion = MagicMock(return_value=(dict(amplitude=1e-2,
@@ -292,7 +304,8 @@ class TestTimeDomainStrainMethod(unittest.TestCase):
                                                                                     psi=2.659, c=None, d=None),
                                                                                ['c', 'd']))
         try:
-            self.waveform_generator.time_domain_strain()
+            self.waveform_generator.time_domain_strain(
+                parameters=self.simulation_parameters)
         except RuntimeError:
             pass
         self.assertListEqual(sorted(self.waveform_generator.parameters.keys()),
