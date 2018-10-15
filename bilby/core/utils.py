bc537259f20ec4a90cb7503c4925ad37dbdceb05
Gregory Ashton <gregory.ashton@ligo.org>
2018-10-15 19:34:10 +1100

    Move `run_command_line` to the core utils

--- bilby/core/utils.py
+++ bilby/core/utils.py
@@ -6,6 +6,7 @@ from math import fmod
 import argparse
 import traceback
 import inspect
+import subprocess
 
 logger = logging.getLogger('bilby')
 
@@ -615,6 +616,38 @@ def derivatives(vals, func, releps=1e-3, abseps=None, mineps=1e-9, reltol=1e-3,
         count += 1
 
     return grads
+
+
+def run_commandline(cl, log_level=20, raise_error=True, return_output=True):
+    """Run a string cmd as a subprocess, check for errors and return output.
+
+    Parameters
+    ----------
+    cl: str
+        Command to run
+    log_level: int
+        See https://docs.python.org/2/library/logging.html#logging-levels,
+        default is '20' (INFO)
+
+    """
+
+    logger.log(log_level, 'Now executing: ' + cl)
+    if return_output:
+        try:
+            out = subprocess.check_output(
+                cl, stderr=subprocess.STDOUT, shell=True,
+                universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            logger.log(log_level, 'Execution failed: {}'.format(e.output))
+            if raise_error:
+                raise
+            else:
+                out = 0
+        os.system('\n')
+        return(out)
+    else:
+        process = subprocess.Popen(cl, shell=True)
+        process.communicate()
 
 
 #  Instantiate the default argument parser at runtime
--- bilby/gw/utils.py
+++ bilby/gw/utils.py
@@ -1,12 +1,11 @@
 from __future__ import division
 import os
 import json
-import subprocess
 
 import numpy as np
 
 from ..core.utils import (gps_time_to_gmst, ra_dec_to_theta_phi,
-                          speed_of_light, logger,
+                          speed_of_light, logger, run_commandline,
                           check_directory_exists_and_if_not_mkdir)
 
 try:
@@ -511,38 +510,6 @@ def gw_data_find(observatory, gps_start_time, duration, calibration,
     cl = ' '.join(cl_list)
     run_commandline(cl)
     return output_cache_file
-
-
-def run_commandline(cl, log_level=20, raise_error=True, return_output=True):
-    """Run a string cmd as a subprocess, check for errors and return output.
-
-    Parameters
-    ----------
-    cl: str
-        Command to run
-    log_level: int
-        See https://docs.python.org/2/library/logging.html#logging-levels,
-        default is '20' (INFO)
-
-    """
-
-    logger.log(log_level, 'Now executing: ' + cl)
-    if return_output:
-        try:
-            out = subprocess.check_output(
-                cl, stderr=subprocess.STDOUT, shell=True,
-                universal_newlines=True)
-        except subprocess.CalledProcessError as e:
-            logger.log(log_level, 'Execution failed: {}'.format(e.output))
-            if raise_error:
-                raise
-            else:
-                out = 0
-        os.system('\n')
-        return(out)
-    else:
-        process = subprocess.Popen(cl, shell=True)
-        process.communicate()
 
 
 def save_to_fits(posterior, outdir, label):
