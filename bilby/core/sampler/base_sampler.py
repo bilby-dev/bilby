8437aca1c5b7281b6848035861d6a38c360bd869
Sylvia Biscoveanu <violinasb@yahoo.com>
2019-09-12 15:05:12 -0700

    Change the calls to sample() to use the method of the CorrelatedPriorDict intead of each key

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -284,6 +284,7 @@ class Sampler(object):
         self._check_if_priors_can_be_sampled()
         if isinstance(self.priors, CorrelatedPriorDict):
             theta = self.priors.sample()
+            theta = theta.values()
         else:
             theta = [self.priors[key].sample()
                      for key in self._search_parameter_keys]
@@ -309,6 +310,7 @@ class Sampler(object):
         for _ in range(n_evaluations):
             if isinstance(self.priors, CorrelatedPriorDict):
                 theta = self.priors.sample()
+                theta = theta.values()
             else:
                 theta = [self.priors[key].sample()
                          for key in self._search_parameter_keys]
