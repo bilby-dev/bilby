7ddbcf6da4297e8b92c7fc12988b92d8fafdc100
Colm Talbot <colm.talbot@ligo.org>
2020-03-02 15:36:23 -0600

    Revert "Make initial draw checking more robust"
    This reverts commit 1a35249c353d140a38eb413fb3a93fa20feba2e4

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -439,11 +439,7 @@ class Sampler(object):
         return np.array(unit_cube), np.array(parameters), np.array(likelihood)
 
     def check_draw(self, theta, warning=True):
-        """
-        Checks if the draw will generate an infinite or nan prior or likelihood
-        
-        This also identifies if the likelihood/prior have been passed through
-        `np.nan_to_num`.
+        """ Checks if the draw will generate an infinite prior or likelihood
 
         Parameters
         ----------
@@ -456,12 +452,11 @@ class Sampler(object):
             True if the likelihood and prior are finite, false otherwise
 
         """
-        bad_values = [np.inf, np.nan_to_num(np.inf), np.nan]
-        if abs(self.log_prior(theta)) in bad_values:
+        if np.isinf(self.log_prior(theta)):
             if warning:
                 logger.warning('Prior draw {} has inf prior'.format(theta))
             return False
-        if abs(self.log_likelihood(theta)) in bad_values:
+        if np.isinf(self.log_likelihood(theta)):
             if warning:
                 logger.warning('Prior draw {} has inf likelihood'.format(theta))
             return False
