04398ab3e832d6a0919594f99916bb7760699bf6
Colm Talbot <colm.talbot@ligo.org>
2020-03-02 15:43:45 -0600

    Make prior sampling more robust.

--- bilby/core/sampler/base_sampler.py
+++ bilby/core/sampler/base_sampler.py
@@ -439,7 +439,10 @@ class Sampler(object):
         return np.array(unit_cube), np.array(parameters), np.array(likelihood)
 
     def check_draw(self, theta, warning=True):
-        """ Checks if the draw will generate an infinite prior or likelihood
+        """
+        Checks if the draw will generate an infinite prior or likelihood
+
+        Also catches the output of `numpy.nan_to_num`.
 
         Parameters
         ----------
@@ -452,11 +455,12 @@ class Sampler(object):
             True if the likelihood and prior are finite, false otherwise
 
         """
-        if np.isinf(self.log_prior(theta)):
+        bad_values = [np.inf, np.nan_to_num(np.inf), np.nan]
+        if abs(self.log_prior(theta)) in bad_values:
             if warning:
                 logger.warning('Prior draw {} has inf prior'.format(theta))
             return False
-        if np.isinf(self.log_likelihood(theta)):
+        if abs(self.log_likelihood(theta)) in bad_values:
             if warning:
                 logger.warning('Prior draw {} has inf likelihood'.format(theta))
             return False
