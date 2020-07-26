f777927f74ec0fb6d38fac56789f940b29b04fc3
Gregory Ashton <gregory.ashton@ligo.org>
2020-07-26 14:19:02 +1000

    Add a check that the directory exists to prevent failure

--- bilby/core/sampler/ptemcee.py
+++ bilby/core/sampler/ptemcee.py
@@ -13,7 +13,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from ..utils import logger
+from ..utils import logger, check_directory_exists_and_if_not_mkdir
 from .base_sampler import SamplerError, MCMCSampler
 
 
@@ -517,6 +517,7 @@ class Ptemcee(MCMCSampler):
         sys.exit(self.exit_code)
 
     def write_current_state(self, plot=True):
+        check_directory_exists_and_if_not_mkdir(self.outdir)
         checkpoint(
             self.iteration,
             self.outdir,
