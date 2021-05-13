745eee594974f055e3ce0a19b20f1b401029e234
Moritz Huebner <email@moritz-huebner.de>
2021-05-14 01:12:08 +1000

    Consolidated source suffixes

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,7 @@ templates_path = ['templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-source_suffix = ['.rst', '.md']
-source_suffix = ['.txt', ".rst", ".ipynb"]
+source_suffix = ['.rst', '.md', '.txt', '.ipynb']
 
 # The master toctree document.
 master_doc = 'index'
