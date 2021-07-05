e54810282334ba6a9257c63636198f92ef2ac8da
Gregory Ashton <gregory.ashton@ligo.org>
2021-07-05 16:45:01 +0100

    Fix error in Categorical prior and add tests

--- test/core/prior/analytical_test.py
+++ test/core/prior/analytical_test.py
@@ -15,13 +15,19 @@ class TestCategoricalPrior(unittest.TestCase):
         self.assertTrue(in_prior)
 
     def test_array_sample(self):
-        categorical_prior = bilby.core.prior.Categorical(3)
-        N = 1000
+        ncat = 4
+        categorical_prior = bilby.core.prior.Categorical(ncat)
+        N = 100000
         s = categorical_prior.sample(N)
         zeros = np.sum(s == 0)
         ones = np.sum(s == 1)
         twos = np.sum(s == 2)
-        self.assertEqual(zeros + ones + twos, N)
+        threes = np.sum(s == 3)
+        self.assertEqual(zeros + ones + twos + threes, N)
+        self.assertAlmostEqual(zeros / N, 1 / ncat, places=int(np.log10(np.sqrt(N))))
+        self.assertAlmostEqual(ones / N, 1 / ncat, places=int(np.log10(np.sqrt(N))))
+        self.assertAlmostEqual(twos / N, 1 / ncat, places=int(np.log10(np.sqrt(N))))
+        self.assertAlmostEqual(threes / N, 1 / ncat, places=int(np.log10(np.sqrt(N))))
 
     def test_single_probability(self):
         N = 3
