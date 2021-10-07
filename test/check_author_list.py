e1a4e86dd88db6507bcad9b161bc606e4f5089b2
Maite Mateu-Lucena <maite.mateu-lucena@ligo.org>
2021-10-07 16:05:35 +0000

    Nestcheck test

--- test/check_author_list.py
+++ test/check_author_list.py
@@ -14,10 +14,26 @@ lines = subprocess.check_output(["git", "shortlog", "HEAD", "-sn"]).decode("utf-
 if len(lines) == 0:
     raise Exception("No authors to check against")
 
+
+def remove_accents(raw_text):
+
+    raw_text = re.sub(u"[àáâãäå]", 'a', raw_text)
+    raw_text = re.sub(u"[èéêë]", 'e', raw_text)
+    raw_text = re.sub(u"[ìíîï]", 'i', raw_text)
+    raw_text = re.sub(u"[òóôõö]", 'o', raw_text)
+    raw_text = re.sub(u"[ùúûü]", 'u', raw_text)
+    raw_text = re.sub(u"[ýÿ]", 'y', raw_text)
+    raw_text = re.sub(u"[ß]", 'ss', raw_text)
+    raw_text = re.sub(u"[ñ]", 'n', raw_text)
+
+    return(raw_text)
+
+
 fail_test = False
 for line in lines:
     line = line.replace(".", " ")
     line = re.sub('([A-Z][a-z]+)', r' \1', re.sub('([A-Z]+)', r' \1', line))
+    line = remove_accents(line)
     for element in line.split()[1:]:
         element = element.lower()
         if element not in AUTHORS_list and element not in special_cases:
