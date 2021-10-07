059ebdcf40be97674cc4e214b8c3f6df7ad3baf5
Matthew David Pitkin <matthew.pitkin@ligo.org>
2021-10-07 16:05:54 +0000

    Add the Zeus MCMC sampler into bilby

--- bilby/core/sampler/zeus.py
+++ bilby/core/sampler/zeus.py
@@ -3,7 +3,6 @@ import signal
 import shutil
 import sys
 from collections import namedtuple
-from distutils.version import LooseVersion
 from shutil import copyfile
 
 import numpy as np
@@ -13,11 +12,11 @@ from ..utils import logger, check_directory_exists_and_if_not_mkdir
 from .base_sampler import MCMCSampler, SamplerError
 
 
-class Emcee(MCMCSampler):
-    """bilby wrapper emcee (https://github.com/dfm/emcee)
+class Zeus(MCMCSampler):
+    """bilby wrapper for Zeus (https://zeus-mcmc.readthedocs.io/)
 
     All positional and keyword arguments (i.e., the args and kwargs) passed to
-    `run_sampler` will be propagated to `emcee.EnsembleSampler`, see
+    `run_sampler` will be propagated to `zeus.EnsembleSampler`, see
     documentation for that class for further help. Under Other Parameters, we
     list commonly used kwargs and the bilby defaults.
 
@@ -37,33 +36,51 @@ class Emcee(MCMCSampler):
         autocorrelation time cannot be calculated
     burn_in_act: float
         The number of autocorrelation times to discard as burn-in
-    a: float (2)
-        The proposal scale factor
 
     """
 
     default_kwargs = dict(
-        nwalkers=500, a=2, args=[], kwargs={}, postargs=None, pool=None,
-        live_dangerously=False, runtime_sortingfn=None, lnprob0=None,
-        rstate0=None, blobs0=None, iterations=100, thin=1, storechain=True,
-        mh_proposal=None)
+        nwalkers=500,
+        args=[],
+        kwargs={},
+        pool=None,
+        log_prob0=None,
+        start=None,
+        blobs0=None,
+        iterations=100,
+        thin=1,
+    )
 
-    def __init__(self, likelihood, priors, outdir='outdir', label='label',
-                 use_ratio=False, plot=False, skip_import_verification=False,
-                 pos0=None, nburn=None, burn_in_fraction=0.25, resume=True,
-                 burn_in_act=3, **kwargs):
-        import emcee
-        self.emcee = emcee
+    def __init__(
+        self,
+        likelihood,
+        priors,
+        outdir="outdir",
+        label="label",
+        use_ratio=False,
+        plot=False,
+        skip_import_verification=False,
+        pos0=None,
+        nburn=None,
+        burn_in_fraction=0.25,
+        resume=True,
+        burn_in_act=3,
+        **kwargs
+    ):
+        import zeus
 
-        if LooseVersion(emcee.__version__) > LooseVersion('2.2.1'):
-            self.prerelease = True
-        else:
-            self.prerelease = False
-        super(Emcee, self).__init__(
-            likelihood=likelihood, priors=priors, outdir=outdir,
-            label=label, use_ratio=use_ratio, plot=plot,
-            skip_import_verification=skip_import_verification, **kwargs)
-        self.emcee = self._check_version()
+        self.zeus = zeus
+
+        super(Zeus, self).__init__(
+            likelihood=likelihood,
+            priors=priors,
+            outdir=outdir,
+            label=label,
+            use_ratio=use_ratio,
+            plot=plot,
+            skip_import_verification=skip_import_verification,
+            **kwargs
+        )
         self.resume = resume
         self.pos0 = pos0
         self.nburn = nburn
@@ -73,81 +90,52 @@ class Emcee(MCMCSampler):
         signal.signal(signal.SIGTERM, self.checkpoint_and_exit)
         signal.signal(signal.SIGINT, self.checkpoint_and_exit)
 
-    def _check_version(self):
-        import emcee
-        if LooseVersion(emcee.__version__) > LooseVersion('2.2.1'):
-            self.prerelease = True
-        else:
-            self.prerelease = False
-        return emcee
-
     def _translate_kwargs(self, kwargs):
-        if 'nwalkers' not in kwargs:
+        if "nwalkers" not in kwargs:
             for equiv in self.nwalkers_equiv_kwargs:
                 if equiv in kwargs:
-                    kwargs['nwalkers'] = kwargs.pop(equiv)
-        if 'iterations' not in kwargs:
-            if 'nsteps' in kwargs:
-                kwargs['iterations'] = kwargs.pop('nsteps')
-        if 'threads' in kwargs:
-            if kwargs['threads'] != 1:
-                logger.warning("The 'threads' argument cannot be used for "
-                               "parallelisation. This run will proceed "
-                               "without parallelisation, but consider the use "
-                               "of an appropriate Pool object passed to the "
-                               "'pool' keyword.")
-                kwargs['threads'] = 1
+                    kwargs["nwalkers"] = kwargs.pop(equiv)
+        if "iterations" not in kwargs:
+            if "nsteps" in kwargs:
+                kwargs["iterations"] = kwargs.pop("nsteps")
+
+        # check if using emcee-style arguments
+        if "start" not in kwargs:
+            if "rstate0" in kwargs:
+                kwargs["start"] = kwargs.pop("rstate0")
+        if "log_prob0" not in kwargs:
+            if "lnprob0" in kwargs:
+                kwargs["log_prob0"] = kwargs.pop("lnprob0")
+
+        if "threads" in kwargs:
+            if kwargs["threads"] != 1:
+                logger.warning(
+                    "The 'threads' argument cannot be used for "
+                    "parallelisation. This run will proceed "
+                    "without parallelisation, but consider the use "
+                    "of an appropriate Pool object passed to the "
+                    "'pool' keyword."
+                )
+                kwargs["threads"] = 1
 
     @property
     def sampler_function_kwargs(self):
-        keys = ['lnprob0', 'rstate0', 'blobs0', 'iterations', 'thin',
-                'storechain', 'mh_proposal']
-
-        # updated function keywords for emcee > v2.2.1
-        updatekeys = {'p0': 'initial_state',
-                      'lnprob0': 'log_prob0',
-                      'storechain': 'store'}
+        keys = ["log_prob0", "start", "blobs0", "iterations", "thin", "progress"]
 
         function_kwargs = {key: self.kwargs[key] for key in keys if key in self.kwargs}
-        function_kwargs['p0'] = self.pos0
-
-        if self.prerelease:
-            if function_kwargs['mh_proposal'] is not None:
-                logger.warning("The 'mh_proposal' option is no longer used "
-                               "in emcee v{}, and will be ignored.".format(
-                                   self.emcee.__version__))
-            del function_kwargs['mh_proposal']
-
-            for key in updatekeys:
-                if updatekeys[key] not in function_kwargs:
-                    function_kwargs[updatekeys[key]] = function_kwargs.pop(key)
-                else:
-                    del function_kwargs[key]
 
         return function_kwargs
 
     @property
     def sampler_init_kwargs(self):
-        init_kwargs = {key: value
-                       for key, value in self.kwargs.items()
-                       if key not in self.sampler_function_kwargs}
+        init_kwargs = {
+            key: value
+            for key, value in self.kwargs.items()
+            if key not in self.sampler_function_kwargs
+        }
 
-        init_kwargs['lnpostfn'] = self.lnpostfn
-        init_kwargs['dim'] = self.ndim
-
-        # updated init keywords for emcee > v2.2.1
-        updatekeys = {'dim': 'ndim',
-                      'lnpostfn': 'log_prob_fn'}
-
-        if self.prerelease:
-            for key in updatekeys:
-                if key in init_kwargs:
-                    init_kwargs[updatekeys[key]] = init_kwargs.pop(key)
-
-            oldfunckeys = ['p0', 'lnprob0', 'storechain', 'mh_proposal']
-            for key in oldfunckeys:
-                if key in init_kwargs:
-                    del init_kwargs[key]
+        init_kwargs["logprob_fn"] = self.lnpostfn
+        init_kwargs["ndim"] = self.ndim
 
         return init_kwargs
 
@@ -171,52 +159,57 @@ class Emcee(MCMCSampler):
     @nburn.setter
     def nburn(self, nburn):
         if isinstance(nburn, (float, int)):
-            if nburn > self.kwargs['iterations'] - 1:
-                raise ValueError('Number of burn-in samples must be smaller '
-                                 'than the total number of iterations')
+            if nburn > self.kwargs["iterations"] - 1:
+                raise ValueError(
+                    "Number of burn-in samples must be smaller "
+                    "than the total number of iterations"
+                )
 
         self.__nburn = nburn
 
     @property
     def nwalkers(self):
-        return self.kwargs['nwalkers']
+        return self.kwargs["nwalkers"]
 
     @property
     def nsteps(self):
-        return self.kwargs['iterations']
+        return self.kwargs["iterations"]
 
     @nsteps.setter
     def nsteps(self, nsteps):
-        self.kwargs['iterations'] = nsteps
+        self.kwargs["iterations"] = nsteps
 
     @property
     def stored_chain(self):
-        """ Read the stored zero-temperature chain data in from disk """
+        """Read the stored zero-temperature chain data in from disk"""
         return np.genfromtxt(self.checkpoint_info.chain_file, names=True)
 
     @property
     def stored_samples(self):
-        """ Returns the samples stored on disk """
+        """Returns the samples stored on disk"""
         return self.stored_chain[self.search_parameter_keys]
 
     @property
     def stored_loglike(self):
-        """ Returns the log-likelihood stored on disk """
-        return self.stored_chain['log_l']
+        """Returns the log-likelihood stored on disk"""
+        return self.stored_chain["log_l"]
 
     @property
     def stored_logprior(self):
-        """ Returns the log-prior stored on disk """
-        return self.stored_chain['log_p']
+        """Returns the log-prior stored on disk"""
+        return self.stored_chain["log_p"]
 
     def _init_chain_file(self):
         with open(self.checkpoint_info.chain_file, "w+") as ff:
-            ff.write('walker\t{}\tlog_l\tlog_p\n'.format(
-                '\t'.join(self.search_parameter_keys)))
+            ff.write(
+                "walker\t{}\tlog_l\tlog_p\n".format(
+                    "\t".join(self.search_parameter_keys)
+                )
+            )
 
     @property
     def checkpoint_info(self):
-        """ Defines various things related to checkpointing and storing data
+        """Defines various things related to checkpointing and storing data
 
         Returns
         =======
@@ -228,21 +221,25 @@ class Emcee(MCMCSampler):
 
         """
         out_dir = os.path.join(
-            self.outdir, '{}_{}'.format(self.__class__.__name__.lower(),
-                                        self.label))
+            self.outdir, "{}_{}".format(self.__class__.__name__.lower(), self.label)
+        )
         check_directory_exists_and_if_not_mkdir(out_dir)
 
-        chain_file = os.path.join(out_dir, 'chain.dat')
-        sampler_file = os.path.join(out_dir, 'sampler.pickle')
-        chain_template =\
-            '{:d}' + '\t{:.9e}' * (len(self.search_parameter_keys) + 2) + '\n'
+        chain_file = os.path.join(out_dir, "chain.dat")
+        sampler_file = os.path.join(out_dir, "sampler.pickle")
+        chain_template = (
+            "{:d}" + "\t{:.9e}" * (len(self.search_parameter_keys) + 2) + "\n"
+        )
 
         CheckpointInfo = namedtuple(
-            'CheckpointInfo', ['sampler_file', 'chain_file', 'chain_template'])
+            "CheckpointInfo", ["sampler_file", "chain_file", "chain_template"]
+        )
 
         checkpoint_info = CheckpointInfo(
-            sampler_file=sampler_file, chain_file=chain_file,
-            chain_template=chain_template)
+            sampler_file=sampler_file,
+            chain_file=chain_file,
+            chain_template=chain_template,
+        )
 
         return checkpoint_info
 
@@ -252,11 +249,13 @@ class Emcee(MCMCSampler):
         return self.sampler.chain[:, :nsteps, :]
 
     def checkpoint(self):
-        """ Writes a pickle file of the sampler to disk using dill """
+        """Writes a pickle file of the sampler to disk using dill"""
         import dill
-        logger.info("Checkpointing sampler to file {}"
-                    .format(self.checkpoint_info.sampler_file))
-        with open(self.checkpoint_info.sampler_file, 'wb') as f:
+
+        logger.info(
+            "Checkpointing sampler to file {}".format(self.checkpoint_info.sampler_file)
+        )
+        with open(self.checkpoint_info.sampler_file, "wb") as f:
             # Overwrites the stored sampler chain with one that is truncated
             # to only the completed steps
             self.sampler._chain = self.sampler_chain
@@ -268,25 +267,29 @@ class Emcee(MCMCSampler):
         sys.exit()
 
     def _initialise_sampler(self):
-        self._sampler = self.emcee.EnsembleSampler(**self.sampler_init_kwargs)
+        self._sampler = self.zeus.EnsembleSampler(**self.sampler_init_kwargs)
         self._init_chain_file()
 
     @property
     def sampler(self):
-        """ Returns the emcee sampler object
+        """Returns the Zeus sampler object
 
         If, already initialized, returns the stored _sampler value. Otherwise,
         first checks if there is a pickle file from which to load. If there is
         not, then initialize the sampler and set the initial random draw
 
         """
-        if hasattr(self, '_sampler'):
+        if hasattr(self, "_sampler"):
             pass
         elif self.resume and os.path.isfile(self.checkpoint_info.sampler_file):
             import dill
-            logger.info("Resuming run from checkpoint file {}"
-                        .format(self.checkpoint_info.sampler_file))
-            with open(self.checkpoint_info.sampler_file, 'rb') as f:
+
+            logger.info(
+                "Resuming run from checkpoint file {}".format(
+                    self.checkpoint_info.sampler_file
+                )
+            )
+            with open(self.checkpoint_info.sampler_file, "rb") as f:
                 self._sampler = dill.load(f)
             self._set_pos0_for_resume()
         else:
@@ -296,13 +299,12 @@ class Emcee(MCMCSampler):
 
     def write_chains_to_file(self, sample):
         chain_file = self.checkpoint_info.chain_file
-        temp_chain_file = chain_file + '.temp'
+        temp_chain_file = chain_file + ".temp"
         if os.path.isfile(chain_file):
             copyfile(chain_file, temp_chain_file)
-        if self.prerelease:
-            points = np.hstack([sample.coords, sample.blobs])
-        else:
-            points = np.hstack([sample[0], np.array(sample[3])])
+
+        points = np.hstack([sample[0], np.array(sample[2])])
+
         with open(temp_chain_file, "a") as ff:
             for ii, point in enumerate(points):
                 ff.write(self.checkpoint_info.chain_template.format(ii, *point))
@@ -310,19 +312,20 @@ class Emcee(MCMCSampler):
 
     @property
     def _previous_iterations(self):
-        """ Returns the number of iterations that the sampler has saved
+        """Returns the number of iterations that the sampler has saved
 
         This is used when loading in a sampler from a pickle file to figure out
         how much of the run has already been completed
         """
         try:
-            return len(self.sampler.blobs)
+            return len(self.sampler.get_blobs())
         except AttributeError:
             return 0
 
     def _draw_pos0_from_prior(self):
         return np.array(
-            [self.get_random_draw_from_prior() for _ in range(self.nwalkers)])
+            [self.get_random_draw_from_prior() for _ in range(self.nwalkers)]
+        )
 
     @property
     def _pos0_shape(self):
@@ -337,8 +340,7 @@ class Emcee(MCMCSampler):
                 self.pos0 = np.squeeze(self.pos0)
 
             if self.pos0.shape != self._pos0_shape:
-                raise ValueError(
-                    'Input pos0 should be of shape ndim, nwalkers')
+                raise ValueError("Input pos0 should be of shape ndim, nwalkers")
             logger.debug("Checking input pos0")
             for draw in self.pos0:
                 self.check_draw(draw)
@@ -347,35 +349,29 @@ class Emcee(MCMCSampler):
             self.pos0 = self._draw_pos0_from_prior()
 
     def _set_pos0_for_resume(self):
-        self.pos0 = self.sampler.chain[:, -1, :]
+        self.pos0 = self.sampler.get_last_sample()
 
     def run_sampler(self):
-        from tqdm.auto import tqdm
         sampler_function_kwargs = self.sampler_function_kwargs
-        iterations = sampler_function_kwargs.pop('iterations')
+        iterations = sampler_function_kwargs.pop("iterations")
         iterations -= self._previous_iterations
 
-        if self.prerelease:
-            sampler_function_kwargs['initial_state'] = self.pos0
-        else:
-            sampler_function_kwargs['p0'] = self.pos0
+        sampler_function_kwargs["start"] = self.pos0
 
         # main iteration loop
-        for sample in tqdm(
-                self.sampler.sample(iterations=iterations, **sampler_function_kwargs),
-                total=iterations):
+        for sample in self.sampler.sample(
+            iterations=iterations, **sampler_function_kwargs
+        ):
             self.write_chains_to_file(sample)
         self.checkpoint()
 
         self.result.sampler_output = np.nan
-        self.calculate_autocorrelation(
-            self.sampler.chain.reshape((-1, self.ndim)))
+        self.calculate_autocorrelation(self.sampler.chain.reshape((-1, self.ndim)))
         self.print_nburn_logging_info()
 
         self._generate_result()
 
-        self.result.samples = self.sampler.chain[:, self.nburn:, :].reshape(
-            (-1, self.ndim))
+        self.result.samples = self.sampler.get_chain(flat=True, discard=self.nburn)
         self.result.walkers = self.sampler.chain
         return self.result
 
@@ -386,10 +382,10 @@ class Emcee(MCMCSampler):
             raise SamplerError(
                 "The run has finished, but the chain is not burned in: "
                 "`nburn < nsteps` ({} < {}). Try increasing the "
-                "number of steps.".format(self.result.nburn, self.nsteps))
-        blobs = np.array(self.sampler.blobs)
-        blobs_trimmed = blobs[self.nburn:, :, :].reshape((-1, 2))
-        log_likelihoods, log_priors = blobs_trimmed.T
+                "number of steps.".format(self.result.nburn, self.nsteps)
+            )
+        blobs = np.array(self.sampler.get_blobs(flat=True, discard=self.nburn)).reshape((-1, 2))
+        log_likelihoods, log_priors = blobs.T
         self.result.log_likelihood_evaluations = log_likelihoods
         self.result.log_prior_evaluations = log_priors
         self.result.log_evidence = np.nan
