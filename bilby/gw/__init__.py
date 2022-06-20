6bdefc3bf1950c0ec5bc93b24dd3fdce48aaa37f
Gregory Ashton <gregory.ashton@ligo.org>
2022-06-20 20:15:05 +0000

    Resolve "`bilby.gw.conversion` module for waveforms which use a custom reference frequency"

--- bilby/gw/__init__.py
+++ bilby/gw/__init__.py
@@ -1,6 +1,6 @@
 from . import (conversion, cosmology, detector, eos, likelihood, prior,
                result, source, utils, waveform_generator)
-from .waveform_generator import WaveformGenerator
+from .waveform_generator import WaveformGenerator, LALCBCWaveformGenerator
 from .likelihood import GravitationalWaveTransient
 from .detector import calibration
 
