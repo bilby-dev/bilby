fae45b6c1bb6cc4360b1ddc5e0699ede1df13a28
Jacob Golomb <jacob.golomb@ligo.org>
2022-09-27 20:16:43 +0000

    Update bilby/gw/conversion.py

--- bilby/gw/conversion.py
+++ bilby/gw/conversion.py
@@ -192,7 +192,7 @@ def convert_to_lal_binary_black_hole_parameters(parameters):
 
     converted_parameters = parameters.copy()
     original_keys = list(converted_parameters.keys())
-    if 'luminosity_distance' not in converted_parameters.keys():
+    if 'luminosity_distance' not in original_keys:
         if 'redshift' in converted_parameters.keys():
             converted_parameters['luminosity_distance'] = \
                 redshift_to_luminosity_distance(parameters['redshift'])
