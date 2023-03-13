00280726d41ab607beb958a392742e32b6d29f9c
Gregory Ashton <gregory.ashton@ligo.org>
2023-03-13 21:31:36 +0000

    MAINT: update solar mass definition

--- bilby/core/utils/constants.py
+++ bilby/core/utils/constants.py
@@ -2,6 +2,6 @@
 
 speed_of_light = 299792458.0  # m/s
 parsec = 3.085677581491367e+16  # m
-solar_mass = 1.9884099021470415e+30  # Kg
+solar_mass = 1.988409870698050731911960804878414216e30  # Kg
 radius_of_earth = 6378136.6  # m
 gravitational_constant = 6.6743e-11  # m^3 kg^-1 s^-2
