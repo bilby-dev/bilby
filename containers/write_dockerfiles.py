45524b5d7800bff14f081040600d303298556346
Colm Talbot <colm.talbot@ligo.org>
2024-08-07 14:40:33 +0000

    MAINT: drop py39 support

--- containers/write_dockerfiles.py
+++ containers/write_dockerfiles.py
@@ -3,7 +3,7 @@ from datetime import date
 with open("dockerfile-template", "r") as ff:
     template = ff.read()
 
-python_versions = [(3, 9), (3, 10), (3, 11)]
+python_versions = [(3, 10), (3, 11)]
 today = date.today().strftime("%Y%m%d")
 
 for python_major_version, python_minor_version in python_versions:
