d669954ae5989cd186ed00090a9391c41b31bb28
Michael J. Williams <michaeljw1@googlemail.com>
2025-04-17 09:57:49 +0100

    DOC: add API doc for `bilby.core.utils.random`

--- bilby/core/utils/random.py
+++ bilby/core/utils/random.py
@@ -1,3 +1,31 @@
+"""Submodule for random number generation.
+
+This module provides a wrapper around numpy's random number generator
+to ensure that the same random number generator is used throughout
+:code:`bilby`. It also provides a function to set the random seed and
+generate random seeds.
+
+The intended usage is to import the submodule and the use the :code:`rng`
+attribute to generate random numbers. For example:
+
+.. code-block:: python
+    from bilby.core.utils import random
+
+    # Seed the random number generator
+    random.seed(1234)
+
+    # Generate a random number between 0 and 1
+    x = random.rng.random()
+
+The :code:`rng` attribute is a :code:`numpy.random.Generator` object, for
+more details see the numpy documentation:
+https://numpy.org/doc/stable/reference/random/generator.html
+
+.. warning::
+    Do not import :code:`rng` directly from :code:`bilby.core.utils.random`
+    since it will not be seeded correctly.
+"""
+
 from numpy.random import default_rng, SeedSequence
 
 
@@ -12,6 +40,15 @@ class Generator:
 
 
 def seed(seed):
+    """Seed the random number generator.
+
+    Also updates the global meta data with the new seed and generator.
+
+    Parameters
+    ----------
+    seed : {None, int, array_like[ints], SeedSequence, BitGenerator, Generator, RandomState}
+        The seed to use for the random number generator.
+    """
     from .meta_data import global_meta_data
 
     Generator.rng = default_rng(seed)
@@ -20,4 +57,16 @@ def seed(seed):
 
 
 def generate_seeds(nseeds):
+    """Generate a list of random seeds.
+
+    Parameters
+    ----------
+    nseeds : int
+        The number of seeds to generate.
+
+    Returns
+    -------
+    numpy.random.SeedSequence
+        A SeedSequence object containing the generated seeds.
+    """
     return SeedSequence(Generator.rng.integers(0, 2**63 - 1, size=4)).spawn(nseeds)
