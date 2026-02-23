6fbbf40908c5cb55210710a973841c78f07d6f7c
Colm Talbot <talbotcolm@gmail.com>
2026-02-23 10:40:49 -0500

    BLD: update python versions for CI containers

--- containers/write_dockerfiles.py
+++ containers/write_dockerfiles.py
@@ -3,7 +3,7 @@ from datetime import date
 with open("dockerfile-template", "r") as ff:
     template = ff.read()
 
-python_versions = [(3, 10), (3, 11), (3, 12)]
+python_versions = [(3, 11), (3, 12), (3, 13), (3, 14)]
 today = date.today().strftime("%Y%m%d")
 
 for python_major_version, python_minor_version in python_versions:
