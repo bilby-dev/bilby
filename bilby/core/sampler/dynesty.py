61cc638618e79b318f68277defffd637989e9844
jasper.martins <jasper.martins@aei.mpg.de>
2026-04-01 09:58:53 +0000

    fix passing of proposal method

--- bilby/core/sampler/dynesty.py
+++ bilby/core/sampler/dynesty.py
@@ -266,6 +266,7 @@ class Dynesty(NestedSampler):
             periodic=self.kwargs.get("periodic", None),
             reflective=self.kwargs.get("reflective", None),
             maxmcmc=self.maxmcmc,
+            proposals=self.proposals,
         )
 
         if kwargs["sample"] == "act-walk":
