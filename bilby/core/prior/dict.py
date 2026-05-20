cef9ebcb15398a0953b6432b56f40b648437b6a1
Michael J. Williams <michaeljw1@googlemail.com>
2026-05-20 14:45:21 +0100

    ENH: raise an error if constraint key is missing from samples

--- bilby/core/prior/dict.py
+++ bilby/core/prior/dict.py
@@ -62,13 +62,29 @@ class PriorDict(dict):
 
     @xp_wrap
     def evaluate_constraints(self, sample, *, xp=None):
+        """Evaluate the constraints for a given sample.
+
+        Applies the conversion function to the sample and evaluates the
+        constraints on the converted sample.
+
+        Raises
+        ======
+        ValueError:
+            If a constraint parameter is not present in the sample after
+            conversion.
+        """
         out_sample = self.conversion_function(sample)
         try:
             prob = xp.ones_like(next(iter(out_sample.values())), dtype=bool)
         except TypeError:
             prob = xp.ones_like(out_sample, dtype=bool)
         for key in self:
-            if isinstance(self[key], Constraint) and key in out_sample:
+            if isinstance(self[key], Constraint):
+                if key not in out_sample:
+                    raise ValueError(
+                        f"Constraint {key} is not present in the sample. "
+                        "Cannot evaluate constraints."
+                    )
                 prob *= self[key].prob(out_sample[key])
         return prob
 
